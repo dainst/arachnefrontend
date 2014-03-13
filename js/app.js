bdeacf8dc87abfd4383b1754823cde1e38efa8a6
fabianzwodrei <fabian@zwodrei.com>
2014-03-13 12:43:05 +0100

    added filter

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
 		.when('/bookmarks', {templateUrl: 'partials/bookmarks.html'})
 		.when('/entity/:id?', {templateUrl: 'partials/entity.html'})
 		.when('/search/:params?', {templateUrl: 'partials/search.html'})
-		.when('/entity/image/:id?', {templateUrl: 'partials/image.html'})	
+		.when('/entity/image/:id?', {templateUrl: 'partials/image.html'})
+		.when('/filter/:params?', {templateUrl: 'partials/filter.html'})	
 		.when('/map', {templateUrl: 'partials/map.html'});
 }]);
