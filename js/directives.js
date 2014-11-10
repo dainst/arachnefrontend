a57492d10846b8c0617dcd79b282463079d8ce18
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-11-10 14:20:17 +0100

    implemented image slider

--- js/directives.js
+++ js/directives.js
@@ -13,7 +13,7 @@ angular.module('arachne.directives', [])
 			restrict: 'A',
 			link: function(scope, element, attrs) {
 
-				if (element[0].tagName== 'IMG') {
+				scope.loadImg = function() {
 					var img = element[0];
 					var imgUri = arachneSettings.dataserviceUri + "/image/";
 					if (scope.imgWidth) {
@@ -27,9 +27,68 @@ angular.module('arachne.directives', [])
             				img.src = window.URL.createObjectURL(blob);
 						}
 					);
+				};
+
+				if (element[0].tagName == 'IMG') {
+					scope.loadImg();
 				} else {
 					console.log("Warning: ar-img directive used on a non img element!");
 				}
+
+				scope.$watch('imgId', function() {
+					scope.loadImg();
+				})
+
+			}
+		}
+	}])
+
+	.directive('arImageslider', [function(){
+		return {
+			scope: { entity: '=', currentQuery: '=' },
+			templateUrl: 'partials/directives/ar-imageslider.html',
+			link: function(scope, element, attrs) {
+
+				var thumbRow = angular.element(angular.element(element.children()[0]).children()[0]).children()[2];
+				var sliderRow = angular.element(element.children()[0]).children()[0];
+
+				scope.currentImgNo = 0;
+				scope.offset = 0;
+				scope.max = 50;
+
+				scope.pageThumbsLeft = function() {
+					var rowRect = sliderRow.getBoundingClientRect();
+					var offset = scope.offset - rowRect.width;
+					if (offset > 0) {
+						scope.offset = offset;
+					} else {
+						scope.offset = 0;
+					}
+				};
+
+				scope.pageThumbsRight = function() {
+					var rowRect = sliderRow.getBoundingClientRect();
+					var offset = scope.offset + rowRect.width;
+					scope.max = thumbRow.getBoundingClientRect().width - rowRect.width;
+					if (offset < scope.max) {
+						scope.offset = offset;
+					} else {
+						scope.offset = scope.max;
+					}
+				};
+
+				scope.setImage = function(imgNo) {
+					scope.currentImgNo = imgNo;
+					var rowRect = sliderRow.getBoundingClientRect();
+					var thumbEl = angular.element(thumbRow).find('img')[imgNo];
+					var thumbRect = thumbEl.getBoundingClientRect();
+					var relOffset = thumbRect.left - rowRect.left;
+					if (relOffset < 0) {
+						scope.offset += relOffset;
+					} else if (relOffset + thumbRect.width > rowRect.width) {
+						scope.offset += relOffset + thumbRect.width - rowRect.width;
+					}
+				};
 
 			}
 		}
