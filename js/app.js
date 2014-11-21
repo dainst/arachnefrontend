ac978f5bc92a803ebd24ff3fc8f35ea8fcfa4329
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-11-21 14:33:16 +0100

    reverted app.js to generic config

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ angular.module('arachne',
 		.when('/allCategories', {templateUrl: 'partials/allCategories.html'})
 		.when('/register', {templateUrl: 'partials/register.html'});
 }]).constant('arachneSettings', {
-		dataserviceUri: "http://lakota.archaeologie.uni-koeln.de/data",
+		dataserviceUri: "http://" + document.location.host + "/data",
 		serverUri : "http://" + document.location.host + document.getElementById('baseLink').getAttribute("href"),
 		openFacets : ["facet_image", "facet_kategorie"]
 }).run(['$rootScope', function($rootScope) {
