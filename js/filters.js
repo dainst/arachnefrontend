32d6630d5a4c2c5d23029fc3dd25ae22c8e62c46
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-11-25 14:03:03 +0100

    convert newlines to <br> in section values (closes #1543)

--- js/filters.js
+++ js/filters.js
@@ -76,6 +76,14 @@ angular.module('arachne.filters', [])
 			return images;
 		}
 	}])
+	.filter('nl2br', function($sce){
+	    return function(msg,is_xhtml) { 
+	        var is_xhtml = is_xhtml || true;
+	        var breakTag = (is_xhtml) ? '<br />' : '<br>';
+	        var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+	        return $sce.trustAsHtml(msg);
+	    }
+	})
 	.filter('base64', function() {
 	    var keyStr = 'ABCDEFGHIJKLMNOP' +
 	        'QRSTUVWXYZabcdef' +
