f1693b97688b6cd53996a754f3c1fb9c0b1b71c1
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-12-05 11:07:40 +0100

    fixed stripCoords filter (closes #1418)

--- js/filters.js
+++ js/filters.js
@@ -28,7 +28,7 @@ angular.module('arachne.filters', [])
 	.filter('stripCoords', function() {
 		return function(value) {
 			if (value.indexOf("[") != -1 && value.charAt(value.length-1) == "]") {
-				return value.substr(0, value.indexOf("[")-1);
+				return value.substr(0, value.indexOf("["));
 			} else {
 				return value;
 			}
