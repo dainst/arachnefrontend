6bc5e31049ebf855019d363c75a1b74f0affcd7f
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-12-17 10:03:20 +0100

    prevent http get in ar-img when img-id not set

--- js/directives.js
+++ js/directives.js
@@ -28,22 +28,29 @@ angular.module('arachne.directives', [])
 
 				scope.loadImg = function() {
 					var img = element[0];
-					var imgUri = arachneSettings.dataserviceUri + "/image/";
-					if (scope.imgWidth) {
-						imgUri += "width/" + scope.imgId + "?width=" + scope.imgWidth;
-					} else {
-						imgUri += "height/" + scope.imgId + "?height=" + scope.imgHeight;
-					}
-					$http.get(imgUri, { responseType: 'arraybuffer' })
-						.success(function(data) {
-							var blob = new Blob([data], {type: 'image/jpeg'});
-            				img.src = window.URL.createObjectURL(blob);
-						}).error(function(result) {
-							img.src = 'img/imagePlaceholder.png';
-							if (scope.imgWidth) img.width = scope.imgWidth;
-							if (scope.imgHeight) img.height = scope.imgHeight;
+					if (scope.imgId) {
+						var img = element[0];
+						var imgUri = arachneSettings.dataserviceUri + "/image/";
+						if (scope.imgWidth) {
+							imgUri += "width/" + scope.imgId + "?width=" + scope.imgWidth;
+						} else {
+							imgUri += "height/" + scope.imgId + "?height=" + scope.imgHeight;
 						}
-					);
+						$http.get(imgUri, { responseType: 'arraybuffer' })
+							.success(function(data) {
+								var blob = new Blob([data], {type: 'image/jpeg'});
+	            				img.src = window.URL.createObjectURL(blob);
+							}).error(function(result) {
+								img.src = 'img/imagePlaceholder.png';
+								if (scope.imgWidth) img.width = scope.imgWidth;
+								if (scope.imgHeight) img.height = scope.imgHeight;
+							}
+						);
+					} else {
+						img.src = 'img/imagePlaceholder.png';
+						if (scope.imgWidth) img.width = scope.imgWidth;
+						if (scope.imgHeight) img.height = scope.imgHeight;
+					}
 				};
 
 				if (element[0].tagName == 'IMG') {
