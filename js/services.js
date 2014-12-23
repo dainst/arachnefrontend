3768a6ae91d4e0693783d99807c89606f1b6ace1
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2014-12-23 14:46:35 +0100

    better handling of ImageProperties.xml request

--- js/services.js
+++ js/services.js
@@ -255,8 +255,8 @@ angular.module('arachne.services', [])
 	})
 
 	// resource interface for backend requests to entity- and search-endpoints
-	.factory('Entity', ['$resource', 'arachneSettings',
-		function($resource, arachneSettings) {
+	.factory('Entity', ['$resource', 'arachneSettings', '$q',
+		function($resource, arachneSettings, $q) {
 
 			return $resource(
 				arachneSettings.dataserviceUri + "/:endpoint/:id",
@@ -281,22 +281,28 @@ angular.module('arachne.services', [])
 					imageProperties: {
 						method: 'GET',
 						url: arachneSettings.dataserviceUri + '/image/zoomify/:id/ImageProperties.xml',
-						transformResponse : function (data) {
-							if(data) {
-								var properties = {};
-								if (window.DOMParser) {
-									var parser = new DOMParser();
-									properties = parser.parseFromString(data,"text/xml");
-								} else {
-									properties = new ActiveXObject("Microsoft.XMLDOM");
-									properties.async=false;
-									properties.loadXML(data);
+						cache: false,
+						interceptor: {
+							response: function(response) {
+								var data = response.data;
+								console.log(data);
+								if(data) {
+									var properties = {};
+									if (window.DOMParser) {
+										var parser = new DOMParser();
+										properties = parser.parseFromString(data,"text/xml");
+									} else {
+										properties = new ActiveXObject("Microsoft.XMLDOM");
+										properties.async=false;
+										properties.loadXML(data);
+									}
+									if(properties.firstChild)
+									return {
+										width : properties.firstChild.getAttribute('WIDTH'),
+										height : properties.firstChild.getAttribute('HEIGHT'),
+										tilesize : properties.firstChild.getAttribute('TILESIZE')
+									};
 								}
-								return {
-									width : properties.firstChild.getAttribute('WIDTH'),
-									height : properties.firstChild.getAttribute('HEIGHT'),
-									tilesize : properties.firstChild.getAttribute('TILESIZE')
-								};
 							}
 						}
 					}
