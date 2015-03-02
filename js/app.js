5386e93be20a1acf17543df47ebbe2a927546669
Chris <ch.bensch@gmx.de>
2015-03-02 16:07:30 +0100

    appjs fix

--- js/app.js
+++ js/app.js
@@ -22,7 +22,6 @@ angular.module('arachne',
 	$routeProvider
 		.when('/', {templateUrl: 'partials/startSite.html'})
 		.when('/catalog', {templateUrl: 'partials/catalog.html'})
-		.when('/bookmarks', {templateUrl: 'partials/bookmarks.html'})
 		.when('/entity/:id?', {templateUrl: 'partials/entity.html'})
 		.when('/entity/:entityId/images', {templateUrl: 'partials/entity_images.html'})
 		.when('/entity/:entityId/image/:imageId', {templateUrl: 'partials/entity_image.html'})
