e9a0bf481a1c87e5b32f163a539af669159c2d6c
Simon Hohl <simon.hohl@gmail.com>
2015-04-08 13:52:29 +0200

    Updated con10t-catalog layout.

--- js/widgets/controllers.js
+++ js/widgets/controllers.js
@@ -33,12 +33,13 @@ angular.module('arachne.widgets.controllers', [])
          $anchorScroll();
       }
    }])
-   .controller('CatalogController', ['$scope', "catalogService", "arachneSettings", function($scope, catalogService, arachneSettings){
+   .controller('CatalogController', ['$scope', "catalogService", '$filter', function($scope, catalogService, $filter){
       var catalogElement = document.querySelector("con10t-catalog");
       var slashRegex = /\//g;
 
       $scope.catalog = catalogService.getCatalog(catalogElement.getAttribute('catalog'));
       $scope.isShown = new Map();
+      $scope.i18n = $filter.i18n;
       $scope.escapePath = function(path){
          return path.replace(slashRegex, '\\/');
       };
@@ -46,6 +47,6 @@ angular.module('arachne.widgets.controllers', [])
          $scope.isShown.set(label, !($scope.isShown.get(label)));
       };
       $scope.checkIfShown = function(label){
-         return $scope.isShown.get(label);
+         return $scope.isShown.get(label); // at first load -> undefined, so it gets hidden but: ugly?
       };
     }]);
