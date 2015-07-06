c74e26fa286d94c15f62bce75bb633ea50c7e7ee
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2015-07-06 21:21:17 +0200

    Implement scenarios from docs/feature_localization_con10t.md.

--- js/controllers_project.js
+++ js/controllers_project.js
@@ -5,26 +5,41 @@ angular.module('arachne.controllers')
 /**
  * Handles requests for the state of the document import.
  *
- * Uses: $scope.templateUrl
+ * $scope variables:
+ *   templateUrl
  *
- * @author: Sebastian Cuy
  * @author: Daniel M. de Oliveira
+ * @author: Sebastian Cuy
  */
 .controller('ProjectController', ['$scope', '$routeParams', 'language', '$http',
     function ($scope, $routeParams, language, $http) {
 
+		var GERMAN_LANG = 'de';
+		var ENGLISH_LANG = 'en';
+		
+		var setTemplateUrl = function(lang){
+			$scope.templateUrl=con10tIndexUrl.replace('{LANG}',lang);
+			return $scope.templateUrl;
+		}
 
-        $scope.templateUrl='con10t/de/'+$routeParams.name+'.html';
-        console.log($scope.templateUrl)
+		var con10tIndexUrl = 'con10t/{LANG}/'+$routeParams.name+'.html';		
+		var lang = language.__();
+		if (language.__().substring(0,2)=='de') lang='de';
+		if (language.__().substring(0,2)=='en') lang='en';
+	
 
-        /**$http.get($scope.templateUrl)
-         .success(
-         function(){
-			console.log('success');
+		
+        $http.get(setTemplateUrl(lang))
+         .error(function(){ // primary language not available
+			 
+			if (language.__().substring(0,2)==ENGLISH_LANG) 
+				setTemplateUrl(GERMAN_LANG);
+			else {
+   				$http.get(setTemplateUrl(ENGLISH_LANG))
+   			 	.error(function(){
+   				 	setTemplateUrl(GERMAN_LANG);
+   			 	});
+			 }
          })
-         .error(
-         function(){
-            console.log('error');
-         })*/
     }
 ]);
--- spec/ProjectControllerSpec.js
+++ spec/ProjectControllerSpec.js
@@ -4,30 +4,121 @@
 
 describe ('ProjectController', function() {
 
-    var scope = {}
+	var CON10T_URL = 'con10t/{LANG}/abc.html'
 
-    beforeEach(function(){
+    var scope = {};
+	var $httpBackend;
+	
+	var con10t_da = CON10T_URL.replace('{LANG}','da');
+	var con10t_it = CON10T_URL.replace('{LANG}','it');
+	var con10t_de = CON10T_URL.replace('{LANG}','de');
+	var con10t_en = CON10T_URL.replace('{LANG}','en');
 
+	var prepare = function(primaryLanguage) {
+		
         module('arachne.controllers',function($provide){
-            $provide.constant('$routeParams', { name: 'abc' });
-            $provide.value('language',{__:function(){return 'de';}});
+        	$provide.constant('$routeParams', { name: 'abc' });
+        	$provide.value('language',{__:function(){return primaryLanguage;}});
         });
+    	
+
+    	inject(function($controller,$routeParams,language,_$httpBackend_){
+
+        	$httpBackend=_$httpBackend_;
+        	$controller('ProjectController',{'$scope':scope});
+    	});
+	}
+
+	// === Behaviour Tests ===
+
+    it ('should provide the link to a german page (german user) ',function(){
+
+		prepare('de');
+
+        $httpBackend.expectGET(con10t_de).respond(200,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_de);
     });
+	
+    it ('should provide the link to an english page (british user) ',function(){
 
-    beforeEach(inject(function($controller,$routeParams,language,_$httpBackend_){
+		prepare('en');
 
-        $httpBackend=_$httpBackend_;
-        $controller('ProjectController',{'$scope':scope});
+        $httpBackend.expectGET(con10t_en).respond(200,'');
+		$httpBackend.flush();
 
-    }));
-
-
-    it ('should fail',function(){
-
-        /*$httpBackend.expectGET('con10t/de/abc.html').
-            respond(200,'');
-        $httpBackend.flush();*/
-
-        expect('todo').toBe('todo');
+        expect(scope.templateUrl).toBe(con10t_en);
     });
+	
+    it ('should provide the link to an italian page (italian user) ',function(){
+
+		prepare('it');
+
+        $httpBackend.expectGET(con10t_it).respond(200,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_it);
+    });
+	
+    it ('should provide the link to an english page (italian user, italian translation missing) ',function(){
+
+		prepare('it');
+
+        $httpBackend.expectGET(con10t_it).respond(404,'');
+    	$httpBackend.expectGET(con10t_en).respond(200,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_en);
+    });
+	
+    it ('should provide the link to a german page (italian user, any translation missing) ',function(){
+
+		prepare('it');
+
+        $httpBackend.expectGET(con10t_it).respond(404,'');
+    	$httpBackend.expectGET(con10t_en).respond(404,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_de);
+    });
+	
+    it ('should provide the link to a german page (italian user, any translation missing) ',function(){
+
+		prepare('da');
+
+        $httpBackend.expectGET(con10t_da).respond(404,'');
+    	$httpBackend.expectGET(con10t_en).respond(404,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_de);
+    });
+	
+	
+	// === Unit Tests ===
+	
+    it ('should provide the link to an english page (american user) ',function(){
+
+		prepare('en-US');
+
+        $httpBackend.expectGET(con10t_en).respond(200,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_en);
+    });
+	
+    it ('should provide the link to a german page (austrian user) ',function(){
+
+		prepare('de-AT');
+
+        $httpBackend.expectGET(con10t_de).respond(200,'');
+		$httpBackend.flush();
+
+        expect(scope.templateUrl).toBe(con10t_de);
+    });
+	
+	
+	// TODO german translation missing. for german, english, other
+	// TODO send problem to message service	
+	
 });
