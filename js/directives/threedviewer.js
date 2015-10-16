ebdaeae343caf2cd84716ea2497db1ecee04433d
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-10-16 11:10:20 +0200

    fixed bug that prevented loading of some directives

--- js/directives/threedviewer.js
+++ js/directives/threedviewer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('arachne.directives', [])
+angular.module('arachne.directives')
 
 .directive('threeDViewer', ['$window', '$q', function ($window, $q) {
     return {
