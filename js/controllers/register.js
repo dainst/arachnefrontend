b385a3cdc8107aaf2daff91e97a9a071ef38a50d
Daniel <danielmarreirosdeoliveira@googlemail.com>
2015-11-20 16:06:39 +0100

    Fix broken registration form.

--- js/controllers/register.js
+++ js/controllers/register.js
@@ -3,40 +3,53 @@
 angular.module('arachne.controllers')
 
 /**
-* Register Form
-*/
-.controller('RegisterController', ['$rootScope', '$scope', '$http', '$filter', 'arachneSettings', 'registerService',
-function ($rootScope, $scope, $http, $filter, arachneSettings, registerService) {
+ * Register Form Controller.
+ * @see partials/register.html.
+ */
+.controller('RegisterController', [ '$scope', '$http', '$filter', 'message', 'arachneSettings',
+function ( $scope, $http, $filter, message, arachneSettings) {
 
-    $rootScope.hideFooter = false;
+    /**
+     * Sends the account registration request to the backend
+     * and reports if it was successful. If it was not successful,
+     * the callback will get a message with an explanation.
+     *
+     * @param user the user object.
+     * @return callback(isSuccess:boolean,message:string)
+     */
+    var register= function(user,callback){
 
-    $scope.user = {};
-    $scope.success = false;
-    $scope.error = "";
+        // The original user object will be copied so the original does not get modified.
+        // The copy get send to the backend and will contain the md5 encrypted pwd. If that would
+        // modify the reference which is bound to the scope
+        // and the request would be rejected, the md5 passes would enter
+        // the form and upon a successful request it would be encrypted again and as a result
+        // unusable as it would be different from what the user entered.
+        var newUser= JSON.parse(JSON.stringify(user))
+
+        if (user.password)
+            newUser.password = $filter('md5')(user.password);
+        if (user.passwordValidation)
+            newUser.passwordValidation = $filter('md5')(user.passwordValidation);
+
+        $http.post(arachneSettings.dataserviceUri + "/user/register", newUser, {
+            "headers": { "Content-Type": "application/json" }
+
+        }).success(function(data) {
+            return callback(true,null);
+        }).error(function(data) {
+            return callback(false,data.message);
+        });
+    };
+
 
     $scope.submit = function() {
-        if ($scope.password && $scope.passwordValidation) {
-            $scope.user.password = $filter('md5')($scope.password);
-            $scope.user.passwordValidation = $filter('md5')($scope.passwordValidation);
-        }
-        registerService.sendContact($scope.user,
-            function (data) {
-                $scope.error = "";
-                $scope.success = true;
-            },
-            function (error) {
-                $scope.error = data.message;
-            }
-        );
-    }
-        /*$http.post(arachneSettings.dataserviceUri + "/user/register", $scope.user, {
-         "headers": { "Content-Type": "application/json" }
-         }).success(function(data) {
-         $scope.error = "";
-         $scope.success = true;
-         }).error(function(data) {
-         $scope.error = data.message;
-         });*/
-
-}
-]);
+        register($scope.user,
+            function(isSuccess,msg){
+            if(isSuccess)
+                message.addMessageForCode('register_success', 'success');
+            else
+                message.addMessageForCode(msg, 'danger');
+        });
+    };
+}]);
