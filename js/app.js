55289d815d918b92a6e60a97d7b5aa790fbd0059
Jan Wieners <jan@jan-wieners.de>
2016-04-11 15:36:14 +0200

    Set page title after page change (closes #2471).

--- js/app.js
+++ js/app.js
@@ -30,35 +30,49 @@ angular.module('arachne',[
 
 		$urlRouterProvider.when('', '/');
 		$urlRouterProvider.otherwise('/404');
+		
+		var title = "Arachne";
+		
 		$stateProvider
-			.state('404', { url: '/404', templateUrl: 'partials/404.html'})
-			.state('start', { url: '/', templateUrl: 'partials/startSite.html'})
-			.state('catalogs', { url: '/catalogs', templateUrl: 'partials/catalogs.html'})
-			.state('catalog/:id', { url: '/catalog/:id', templateUrl: 'partials/catalog.html'})
-			.state('entity', { url: '/entity/:id?/:params?', templateUrl: 'partials/entity.html', reloadOnSearch: false})
-			.state('entityImages', { url: '/entity/:entityId/images', templateUrl: 'partials/entity_images.html'})
-			.state('entityImage', { url: '/entity/:entityId/image/:imageId', templateUrl: 'partials/entity_image.html'})
-			.state('search', { url: '/search?q&fq&view&sort&offset&limit&desc', templateUrl: 'partials/search.html'})
-			.state('category', { url: '/category/:category', templateUrl: 'partials/category.html'})	
-			.state('map', { url: '/map?q&fq', templateUrl: 'partials/map.html'})
-			.state('gridmap', { url: '/gridmap', templateUrl: 'partials/gridmap.html'})
-			.state('3d', { url: '/3d', templateUrl: 'partials/3d.html'})
-			.state('allCategories', { url: '/allCategories', templateUrl: 'partials/allCategories.html'})
-			.state('projects', { url: '/projects', templateUrl: 'partials/projects.html'})
-			.state('register', { url: '/register', templateUrl: 'partials/register.html'})
-			.state('editUser', { url: '/editUser', templateUrl: 'partials/editUser.html'})
-			.state('apis', { url: '/apis', templateUrl: 'partials/apis.html'})
-			.state('contact', { url: '/contact', templateUrl: 'partials/contact.html'})
-			.state('dataimport', { url: '/admin/dataimport', templateUrl: 'partials/dataimport.html'})
-			.state('pwdreset', { url: '/pwdreset', templateUrl: 'partials/pwdreset.html'})
-			.state('userActivation', { url: '/user/activation/:token', templateUrl: 'partials/activation.html'})
-			.state('project', { url: '/project/:title?q&fq', templateUrl: 'partials/static.html'})
-			.state('info', { url: '/info/:title', templateUrl: 'partials/static.html'}); // Named it info, not static, to sound not too technical.
+			.state('404', { url: '/404', templateUrl: 'partials/404.html', data: { pageTitle: 'Arachne | 404' }})
+			.state('start', { url: '/', templateUrl: 'partials/startSite.html', data: { pageTitle: title }})
+			.state('catalogs', { url: '/catalogs', templateUrl: 'partials/catalogs.html', data: { pageTitle: title }})
+			.state('catalog/:id', { url: '/catalog/:id', templateUrl: 'partials/catalog.html', data: { pageTitle: title }})
+			.state('entity', { url: '/entity/:id?/:params?', templateUrl: 'partials/entity.html', reloadOnSearch: false, data: { pageTitle: title }})
+			.state('entityImages', { url: '/entity/:entityId/images', templateUrl: 'partials/entity_images.html', data: { pageTitle: title }})
+			.state('entityImage', { url: '/entity/:entityId/image/:imageId', templateUrl: 'partials/entity_image.html', data: { pageTitle: title }})
+			.state('search', { url: '/search?q&fq&view&sort&offset&limit&desc', templateUrl: 'partials/search.html', data: { pageTitle: title }})
+			.state('category', { url: '/category/:category', templateUrl: 'partials/category.html', data: { pageTitle: title }})
+			.state('map', { url: '/map?q&fq', templateUrl: 'partials/map.html', data: { pageTitle: title }})
+			.state('gridmap', { url: '/gridmap', templateUrl: 'partials/gridmap.html', data: { pageTitle: title }})
+			.state('3d', { url: '/3d', templateUrl: 'partials/3d.html', data: { pageTitle: title }})
+			.state('allCategories', { url: '/allCategories', templateUrl: 'partials/allCategories.html', data: { pageTitle: title }})
+			.state('projects', { url: '/projects', templateUrl: 'partials/projects.html', data: { pageTitle: title }})
+			.state('register', { url: '/register', templateUrl: 'partials/register.html', data: { pageTitle: title }})
+			.state('editUser', { url: '/editUser', templateUrl: 'partials/editUser.html', data: { pageTitle: title }})
+			.state('apis', { url: '/apis', templateUrl: 'partials/apis.html', data: { pageTitle: title }})
+			.state('contact', { url: '/contact', templateUrl: 'partials/contact.html', data: { pageTitle: title }})
+			.state('dataimport', { url: '/admin/dataimport', templateUrl: 'partials/dataimport.html', data: { pageTitle: title }})
+			.state('pwdreset', { url: '/pwdreset', templateUrl: 'partials/pwdreset.html', data: { pageTitle: title }})
+			.state('userActivation', { url: '/user/activation/:token', templateUrl: 'partials/activation.html', data: { pageTitle: title }})
+			.state('project', { url: '/project/:title?q&fq', templateUrl: 'partials/static.html', data: { pageTitle: title }})
+			.state('info', { url: '/info/:title', templateUrl: 'partials/static.html', data: { pageTitle: title }}); // Named it info, not static, to sound not too technical.
 	}
-]).constant('arachneSettings', {
-		dataserviceUri: "http://" + document.location.host + "/data",
-		openFacets : ["facet_image", "facet_kategorie", "facet_bestandsname", "facet_subkategoriebestand"],
-		sortableFields : ["entityId", "title", "subtitle"]
+])
+/**
+ * Change <title> after page change
+ * Jan G. Wieners
+ */
+.run(function($rootScope) {
+
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+		document.title = toState.data.pageTitle;
+	});
+})
+.constant('arachneSettings', {
+	dataserviceUri: "http://" + document.location.host + "/data",
+	openFacets : ["facet_image", "facet_kategorie", "facet_bestandsname", "facet_subkategoriebestand"],
+	sortableFields : ["entityId", "title", "subtitle"]
 })
 .constant('componentsSettings', {
 		transl8Uri: "http://bogusman01.dai-cloud.uni-koeln.de/transl8/translation/jsonp?application=arachne4_frontend&lang={LANG}&callback=JSON_CALLBACK"
