a373b5819472245cb8dcbb61b33ac5c3a73b5f97
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-04-22 11:58:37 +0200

    load more button for catalog tree widget (closes #2816)

--- js/widgets/con10t_catalog_tree.js
+++ js/widgets/con10t_catalog_tree.js
@@ -9,6 +9,9 @@ angular.module('arachne.widgets.directives')
  */
 .directive('con10tCatalogTree', ['Catalog', 'CatalogEntry', '$filter',
 	function(Catalog, CatalogEntry, $filter) {
+
+		var limit = 20;
+
 		return {
 			restrict: 'E',
 			scope: {
@@ -17,8 +20,11 @@ angular.module('arachne.widgets.directives')
 			},
 			templateUrl: 'partials/widgets/con10t-catalog-tree.html',
 			link: function(scope) {
-				
-				scope.catalog = Catalog.get({id:scope.catalogId});
+
+                scope.catalog = { root: { loading: true } };
+				Catalog.get({id:scope.catalogId, limit: limit}, function(catalog) {
+                    scope.catalog = catalog;
+                });
 				scope.isShown = {};
 				
 				scope.escapePath = function(path){
@@ -26,11 +32,13 @@ angular.module('arachne.widgets.directives')
 				};
 				
 				scope.toggleCollapse = function(node){
-				    if (node.hasChildren) {
+				    if (node.totalChildren) {
 						scope.isShown[node.id] = !scope.isShown[node.id];
 				    	if (!node.hasOwnProperty('children')) {
-				    		CatalogEntry.get({id:node.id}, function(newNode) {
+                            node.loading = true;
+				    		CatalogEntry.get({id:node.id, limit: limit}, function(newNode) {
 				    			node.children = newNode.children;
+                                node.loading = false;
 				    		});
 				    	}
 				    }
@@ -38,6 +46,14 @@ angular.module('arachne.widgets.directives')
 				
 				scope.checkIfShown = function(node){
 					return scope.isShown[node.id]; // at first load -> undefined, so it gets hidden but: ugly?
+				};
+
+				scope.loadMore = function(node) {
+                    node.loading = true;
+                    CatalogEntry.get({id:node.id, limit: limit, offset: node.children.length}, function(newNode) {
+                        node.children = node.children.concat(newNode.children);
+                        node.loading = false;
+                    });
 				};
 
 			}
