9239e269e44b7c29934821c22396bd14e8b1cbab
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-05-02 16:49:44 +0200

    fixed yet another dependency injection issue

--- js/filters/_filters.js
+++ js/filters/_filters.js
@@ -73,14 +73,14 @@ angular.module('arachne.filters', [])
 			return images;
 		}
 	}])
-	.filter('nl2br', function($sce){
+	.filter('nl2br', ['$sce', function($sce){
 	    return function(msg,is_xhtml) { 
 	        var is_xhtml = is_xhtml || true;
 	        var breakTag = (is_xhtml) ? '<br />' : '<br>';
 	        var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
 	        return $sce.trustAsHtml(msg);
 	    }
-	})
+	}])
 	.filter('base64', function() {
 	    var keyStr = 'ABCDEFGHIJKLMNOP' +
 	        'QRSTUVWXYZabcdef' +
