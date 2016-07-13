635bafeb13285c4dc06759b4590bf53e4ee671fd
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-07-13 11:12:35 +0200

    basic e2e test setup with protractor (closes #3156)

---
+++ e2e/basic-scenarios.spec.js
@@ -0,0 +1,14 @@
+var common = require("./common.js");
+var utils = require("./utils.js");
+
+/*
+ * In order to prevent errors caused by e2e tests running too fast you can slow them down by calling the following
+ * function. Use higher values for slower tests.
+ *
+ * utils.delayPromises(50);
+ *
+ */
+
+describe('arachne 4 frontend', function() {
+
+});
