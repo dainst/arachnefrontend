698f367e470089c7404f97449b9d11b523e3807b
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-07-15 11:40:05 +0200

    updated e2e test to ensure presence of markers in place map

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -1,9 +1,12 @@
+var EC = protractor.ExpectedConditions;
+
 describe('con10t pages', function() {
 
     it('should contain markers on the grako_map page', function() {
     	browser.get('/project/grako_map');
-        var markers = element.all(by.css('.leaflet-marker-icon'));
-        expect(markers.length).toBeGreaterThan(0);
+        var marker = element(by.css('.leaflet-marker-icon'));
+        browser.wait(EC.presenceOf(marker), 10000);
+        expect(marker.isPresent).toBeTruthy();
     });
 
 });
