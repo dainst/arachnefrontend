6243d9c4cb533606c123ad8168ab018a8c699cf3
Jan G. Wieners <jan@jan-wieners.de>
2016-07-21 11:18:04 +0200

    Add e2e test for con10t-images embedded by their arachne entity identifier.

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -13,9 +13,16 @@ describe('con10t pages', function() {
 
 describe('con10t-image', function() {
 
-    fit('should display an image from a given url', function() {
+    it('should display an image from a given url', function() {
         browser.get('/project/steindorff');
         var marker = element(by.css('.con10t-image-fromsrc'));
+        browser.wait(EC.presenceOf(marker), 10000);
+        expect(marker.isPresent()).toBe(true);
+    });
+
+    it('should display an image from an arachne entity identifier', function() {
+        browser.get('/project/schlossfulda');
+        var marker = element(by.css('.con10t-image-fromentityid'));
         browser.wait(EC.presenceOf(marker), 10000);
         expect(marker.isPresent()).toBe(true);
     });
