2bf16ed83fd896e7bb7566df7a8395d38dc887f1
Jan G. Wieners <jan@jan-wieners.de>
2016-07-21 10:48:04 +0200

    Add test for con10t-image widget.

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -10,3 +10,15 @@ describe('con10t pages', function() {
     });
 
 });
+
+describe('con10t-image', function() {
+
+    fit('should display an image from a given url', function() {
+        browser.get('/project/steindorff');
+        var marker = element(by.css('.con10t-image-fromsrc'));
+        browser.wait(EC.presenceOf(marker), 10000);
+        expect(marker.isPresent()).toBe(true);
+    });
+
+});
+
