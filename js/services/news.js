d0e6d587bc5efb5a2ef4fd8191d3dea4394d1085
Jan G. Wieners <jan@jan-wieners.de>
2016-08-22 12:53:40 +0200

    Delete unused dependency.

--- js/services/news.js
+++ js/services/news.js
@@ -2,11 +2,11 @@
 
 angular.module('arachne.services')
 
-.factory('newsFactory', ['$http', 'arachneSettings', '$timeout',
-function($http, arachneSettings){
-    var factory = {};
-    factory.getNews = function() {
-        return $http.get( arachneSettings.dataserviceUri + '/news/de');
-    };
-    return factory;
-}]);
+    .factory('newsFactory', ['$http', 'arachneSettings',
+        function ($http, arachneSettings) {
+            var factory = {};
+            factory.getNews = function () {
+                return $http.get(arachneSettings.dataserviceUri + '/news/de');
+            };
+            return factory;
+        }]);
