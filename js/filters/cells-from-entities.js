6ef4874eddfbdd3a2612469c7f16a9f66023322e
Jan G. Wieners <jan@jan-wieners.de>
2016-08-24 14:08:00 +0200

    Add horizontal ruler after each cell entry (fixes #3332).

--- js/filters/cells-from-entities.js
+++ js/filters/cells-from-entities.js
@@ -2,27 +2,33 @@ angular.module('arachne.filters')
 
     .filter('cellsFromEntities', ['arachneSettings', 'categoryService', function (arachneSettings, categoryService) {
         return function (entities, query) {
+
             for (var i in entities) {
+                
                 entities[i].label = categoryService.getSingular(entities[i].type);
                 entities[i].href = 'entity/' + entities[i].entityId;
+
                 if (typeof query != 'undefined') {
                     entities[i].href += query.setParam("resultIndex", (parseInt(query.offset) + parseInt(i) + 1)).toString();
                 }
                 if (typeof entities[i].thumbnailId != 'undefined') {
                     entities[i].imgUri = arachneSettings.dataserviceUri + "/image/height/" + entities[i].thumbnailId + "?height=300";
                 }
+
                 if (typeof entities[i].highlights != 'undefined') {
-                    var highlights = []
+
+                    var highlights = [];
                     for (var highlightedField in entities[i].highlights) {
-                        if (highlightedField == "subtitle" || highlightedField == "title") {
+
+                        if (highlightedField === "subtitle" || highlightedField === "title") {
                             entities[i][highlightedField] = entities[i].highlights[highlightedField].join('...')
                         } else {
-                            highlights.push(entities[i].highlights[highlightedField].join('...'))
+                            highlights.push(entities[i].highlights[highlightedField].join('...<hr>'))
                         }
                     }
-                    entities[i].highlighting = highlights.join(', ')
+                    entities[i].highlighting = highlights.join(', ');
                 }
             }
             return entities;
         }
-    }])
+    }]);
