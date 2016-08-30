bb4092730ff9655a0755df0ea9a9d522e112b502
Daniel Marreiros de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2016-08-30 10:48:11 +0200

    Set up initial version of heatmap.

---
+++ js/services/heatmap.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('arachne.services')
+
+/**
+ * @author Daniel de Oliveira
+ * @author Sebastian Cuy
+ */
+.factory('heatmapService', function () {
+
+    function heatMapColorForValue(value) {
+        var h = Math.round((1.0 - value) * 240);
+        return "hsl(" + h + ", 90%, 50%)";
+    }
+
+    function generateGradient(s) {
+        var gradient = {};
+        for (var i = 1; i <= 10; i++) {
+            gradient[s * i / 10] = heatMapColorForValue(i / 10);
+        }
+        return gradient;
+    }
+
+    function max(boxesToDraw) {
+        var max=0;
+        for (var i = 0; i < boxesToDraw.length; i++) {
+            if ((boxesToDraw[i].count)>max) max=boxesToDraw[i].count;
+        }
+        return max;
+    }
+
+    var heatPoints= function(boxesToDraw) {
+
+        var heatPoints=[];
+        for (var i = 0; i < boxesToDraw.length; i++) {
+            var coords = angular.fromJson(boxesToDraw[i].value);
+            coords.push(boxesToDraw[i].count);
+            heatPoints.push(coords);
+        }
+        return heatPoints;
+    };
+
+    var drawBuckets = function(ghprec,bbox,bucketsToDraw,map){
+        if (!bucketsToDraw) return;
+
+        var hps=heatPoints(bucketsToDraw);
+
+        L.heatLayer(hps, {
+            radius: ghprec*25,
+            max: max(bucketsToDraw),
+            gradient: generateGradient(0.7),
+            minOpacity: 0.3
+        }).addTo(map);
+    };
+
+    return {
+        drawBuckets: function (ghprec,bbox,bucketsToDraw,map) {
+            return drawBuckets(ghprec,bbox,bucketsToDraw,map);
+        }
+    }
+});
--- js/services/map.js
+++ js/services/map.js
@@ -7,265 +7,248 @@ angular.module('arachne.services')
  * Provides convenience methods to alter the map's state.
  *
  * @author: David Neugebauer
- * @author: Daniel M. de Oliveira
+ * @author: Daniel de Oliveira
  */
-    .factory('mapService', ['searchService', function (searchService) {
+.factory('mapService', ['searchService', function (searchService) {
 
-        var map = null;
-        var overlays = null; // { key: LayerConfig }
-        var activeOverlays = {}; // { key: TileLayer }
+    var map = null;
+    var overlays = null; // { key: LayerConfig }
+    var activeOverlays = {}; // { key: TileLayer }
 
-        var baselayers = {
-            'osm': {
-                'name': 'OpenStreetMap',
-                'type': 'xyz',
-                'url': 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
-                'yayerOptions': {
-                    'subdomains': ['a', 'b', 'c'],
-                    'attribution': '&copy; <a href=\'http://www.opencyclemap.org\'>OpenCycleMap</a>, &copy; <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors',
-                    'continuousWorld': false,
-                    'maxZoom': 18
-                }
-            }
-        }; // { key: LayerConfig }
-        var activeBaselayer = 'osm'; // TileLayer
-        var activeBaselayerKey = "";
-
-        var boxesListener = null;
-        var sizeListener = null;
-        var queryListener = null;
-
-        var _getMapQuery = function (query, stripExtraParams) {
-            var newQuery = query.removeParams('lat', 'lng', 'zoom', 'overlays', 'baselayers');
-
-            newQuery.zoom = map.getZoom();
-            newQuery.lat = map.getCenter().lat;
-            newQuery.lng = map.getCenter().lng;
-
-            var overlays = [];
-            for (var key in activeOverlays) {
-                overlays.push(key);
-            }
-            if (overlays.length > 0) {
-                newQuery.overlays = overlays;
-            }
-
-            if (activeBaselayerKey != "") {
-                newQuery.baselayer = activeBaselayerKey;
-            }
-
-            if (stripExtraParams)
-                return query.removeParams(['lat', 'lng', 'zoom', 'bbox', 'ghprec', 'baselayer']);
-            return newQuery;
-        };
-
-
-        /**
-         * Guesses a good geohash precision value from the zoomlevel
-         * "zoom". The returned value can be used as "ghprec"-param in
-         * search queries
-         */
-        var getGhprecFromZoom = function () {
-            var zl = map.getZoom();
-
-            var ghprecForZoomLevel =
-                [1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7];
-
-            if (zl > 18) zl = 18;
-            return ghprecForZoomLevel[zl];
-        };
-
-
-        var _bBoxFromBounds = function (bounds) {
-            var southEast = bounds.getSouthEast();
-            var northWest = bounds.getNorthWest();
-            return [northWest.lat, northWest.lng, southEast.lat, southEast.lng].join(',');
-        };
-
-        var feedListenersWithUpdates = function () {
-
-            var cq = searchService.currentQuery();
-            cq.bbox = _bBoxFromBounds(map.getBounds());
-            cq.ghprec = getGhprecFromZoom();
-
-            searchService.markDirty();
-            searchService.getCurrentPage().then(function () {
-
-                var boxesToDraw = null;
-                var agg_geogrid = searchService.getFacet("agg_geogrid");
-                if (agg_geogrid) boxesToDraw = agg_geogrid.values;
-
-                if (queryListener) queryListener(_getMapQuery(searchService.currentQuery()).toString());
-                if (boxesListener) boxesListener(getGhprecFromZoom(), _bBoxFromBounds(map.getBounds()), boxesToDraw);
-                if (sizeListener)  sizeListener(searchService.getSize());
-            });
-        };
-
-        return {
-
-
-            bBoxFromBounds: function (bounds) {
-                return _bBoxFromBounds(bounds);
-            },
-
-            registerQueryListener: function (ql) {
-                queryListener = ql;
-            },
-
-            registerSizeListener: function (sl) {
-                sizeListener = sl;
-            },
-
-            /**
-             * Initialize the Map given an Attribute id of an
-             * HTML-Element.
-             *  Params are the same as those of Learflet's L.map():
-             * http://leafletjs.com/reference.html#map-constructor
-             *
-             * TODO: This thing here should absolutely not return the map object. Instead the service should encapsulate it.
-             * @return the map governed by the mapService.
-             */
-            initializeMap: function (id, options, bl) {
-                map = L.map(id, options);
-                L.Icon.Default.imagePath = 'img';
-
-                // Disable dragging functionality if outside of container bounds
-                L.Draggable.prototype._freeze = false;
-                L.Draggable.prototype._updatePosition = function () {
-                    if (this._freeze) {
-                        return;
-                    }
-
-                    this.fire('predrag');
-                    L.DomUtil.setPosition(this._element, this._newPos);
-                    this.fire('drag');
-                };
-
-                map.on('mouseout', function () {
-                    map.dragging._draggable._freeze = true;
-                });
-                map.on('mouseover', function () {
-                    map.dragging._draggable._freeze = false;
-                });
-                // / Disable dragging functionality if outside of container bounds
-
-                if (bl) {
-                    boxesListener = bl;
-
-                    // Hook for redrawing the grid on zoom and move events
-                    map.on('moveend', function () {
-                        feedListenersWithUpdates();
-                    });
-                }
-
-                // see comment in apidoc above
-                return map;
-            },
-
-            // Returns the map governed by mapService.
-            getMap: function () {
-                return map;
-            },
-
-            // Increases the map's zoom level or decreases it if the
-            // increment is negative.
-            increaseZoom: function (increment) {
-                var lvl = map.getZoom();
-                lvl = lvl + increment;
-                if (lvl < 1) {
-                    lvl = 1;
-                }
-                map.setZoom(lvl);
-            },
-
-            // Sets the overlays available for this map
-            setOverlays: function (ols) {
-
-                var result = {};
-                // overlays are either grouped at .groups
-                if (ols && ols.groups) {
-                    for (var i = 0; i < ols.groups.length; i++) {
-                        var group = ols.groups[i];
-
-                        for (var j = 0; j < group.overlays.length; j++) {
-                            var overlay = group.overlays[j];
-                            result[overlay.key] = overlay;
-                        }
-                    }
-                    // or just listed directly
-                } else if (ols) {
-                    for (var i = 0; i < ols.length; i++) {
-                        var overlay = ols[i];
-                        result[overlay.key] = overlay;
-                    }
-                }
-                overlays=result;
-            },
-            
-            // Sets the baselayers available for this map
-            // { key: LayerConfig, ... }
-            setBaselayers: function (baselayersToSet) {
-                if (baselayersToSet) {
-                    baselayers = baselayersToSet;
-                }
-            },
-
-            // Adds an overlay to the map
-            activateOverlay: function (key) {
-                var layerConfig = overlays[key];
-                if (layerConfig && layerConfig.type == 'wms') {
-                    activeOverlays[key] = L.tileLayer.wms(layerConfig.url, layerConfig.layerOptions);
-                    map.addLayer(activeOverlays[key]);
-                }
-            },
-
-            // Removes the old baselayer if neccessary and sets a new one
-            // identified by it's key
-            activateBaselayer: function (key) {
-                if (activeBaselayer) {
-                    map.removeLayer(activeBaselayer);
-                }
-
-                var layerConfig = baselayers[key];
-                activeBaselayer = L.tileLayer(layerConfig.url, layerConfig.layerOptions);
-                activeBaselayerKey = key;
-                map.addLayer(activeBaselayer);
-            },
-
-            // Removes an overlay from the map
-            // TODO: Keep layer for later refresh
-            deactivateOverlay: function (key) {
-                map.removeLayer(activeOverlays[key]);
-                delete activeOverlays[key];
-            },
-
-            // Toggle an overlay on the map, identified by key
-            toggleOverlay: function (key) {
-                if (activeOverlays[key]) {
-                    this.deactivateOverlay(key);
-                } else {
-                    this.activateOverlay(key);
-                }
-
-            },
-
-            /**
-             * Set map view to center coords with zoomlevel
-             */
-            initializeView: function (lat, lng, zoom) {
-                var lt = lat || 40;
-                var lg = lng || -10;
-                var zm = zoom || 3;
-                map.setView([lt, lg], zm);
-            },
-
-            /**
-             * Returns a Query object copied from currentQuery and
-             * enriched with all parameters needed to recreate the current map
-             * @param stripExtraParams boolean
-             */
-            getMapQuery: function (query, stripExtraParams) {
-                return _getMapQuery(query, stripExtraParams);
+    var baselayers = {
+        'osm': {
+            'name': 'OpenStreetMap',
+            'type': 'xyz',
+            'url': 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
+            'yayerOptions': {
+                'subdomains': ['a', 'b', 'c'],
+                'attribution': '&copy; <a href=\'http://www.opencyclemap.org\'>OpenCycleMap</a>, &copy; <a href=\'http://www.openstreetmap.org/copyright\'>OpenStreetMap</a> contributors',
+                'continuousWorld': false,
+                'maxZoom': 18
             }
         }
-    }]);
+    }; // { key: LayerConfig }
+    var activeBaselayer = 'osm'; // TileLayer
+    var activeBaselayerKey = "";
+
+    var bucketsListener = null;
+    var sizeListener = null;
+    var queryListener = null;
+
+    var _getMapQuery = function (query, stripExtraParams) {
+        var newQuery = query.removeParams('lat', 'lng', 'zoom', 'overlays', 'baselayers');
+
+        newQuery.zoom = map.getZoom();
+        newQuery.lat = map.getCenter().lat;
+        newQuery.lng = map.getCenter().lng;
+
+        var overlays = [];
+        for (var key in activeOverlays) {
+            overlays.push(key);
+        }
+        if (overlays.length > 0) {
+            newQuery.overlays = overlays;
+        }
+
+        if (activeBaselayerKey != "") {
+            newQuery.baselayer = activeBaselayerKey;
+        }
+
+        if (stripExtraParams)
+            return query.removeParams(['lat', 'lng', 'zoom', 'bbox', 'ghprec', 'baselayer']);
+        return newQuery;
+    };
+
+
+    /**
+     * Guesses a good geohash precision value from the zoomlevel
+     * "zoom". The returned value can be used as "ghprec"-param in
+     * search queries
+     */
+    var getGhprecFromZoom = function () {
+        var zl = map.getZoom();
+
+        var ghprecForZoomLevel =
+            [1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7];
+
+        if (zl > 18) zl = 18;
+        return ghprecForZoomLevel[zl];
+    };
+
+
+    var _bBoxFromBounds = function (bounds) {
+        var southEast = bounds.getSouthEast();
+        var northWest = bounds.getNorthWest();
+        return [northWest.lat, northWest.lng, southEast.lat, southEast.lng].join(',');
+    };
+
+    var feedListenersWithUpdates = function () {
+
+        var cq = searchService.currentQuery();
+        cq.bbox = _bBoxFromBounds(map.getBounds());
+        cq.ghprec = getGhprecFromZoom();
+
+        searchService.markDirty();
+        searchService.getCurrentPage().then(function () {
+
+            var bucketsToDraw = null;
+            var agg_geogrid = searchService.getFacet("agg_geogrid");
+            if (agg_geogrid) bucketsToDraw = agg_geogrid.values;
+
+            if (queryListener) queryListener(_getMapQuery(searchService.currentQuery()).toString());
+            if (bucketsListener) bucketsListener(getGhprecFromZoom(), _bBoxFromBounds(map.getBounds()), bucketsToDraw, map);
+            if (sizeListener)  sizeListener(searchService.getSize());
+        });
+    };
+
+    return {
+
+
+        bBoxFromBounds: function (bounds) {
+            return _bBoxFromBounds(bounds);
+        },
+
+        registerQueryListener: function (ql) {
+            queryListener = ql;
+        },
+
+        registerSizeListener: function (sl) {
+            sizeListener = sl;
+        },
+
+        registerBucketsListener: function(bl) {
+            bucketsListener = bl;
+        },
+
+        /**
+         * Initialize the Map given an Attribute id of an
+         * HTML-Element.
+         *  Params are the same as those of Learflet's L.map():
+         * http://leafletjs.com/reference.html#map-constructor
+         *
+         * TODO: This thing here should absolutely not return the map object. Instead the service should encapsulate it.
+         * @return the map governed by the mapService.
+         */
+        initializeMap: function (id, options) {
+            map = L.map(id, options);
+            L.Icon.Default.imagePath = 'img';
+
+            // Disable dragging functionality if outside of container bounds
+            L.Draggable.prototype._freeze = false;
+            L.Draggable.prototype._updatePosition = function () {
+                if (this._freeze) {
+                    return;
+                }
+
+                this.fire('predrag');
+                L.DomUtil.setPosition(this._element, this._newPos);
+                this.fire('drag');
+            };
+
+            map.on('mouseout', function () {
+                map.dragging._draggable._freeze = true;
+            });
+            map.on('mouseover', function () {
+                map.dragging._draggable._freeze = false;
+            });
+            // / Disable dragging functionality if outside of container bounds
+
+
+            // Hook for redrawing the grid on zoom and move events
+            map.on('moveend', function () {
+                feedListenersWithUpdates();
+            });
+
+            // see comment in apidoc above
+            return map;
+        },
+
+        // Returns the map governed by mapService.
+        getMap: function () {
+            return map;
+        },
+
+        // Increases the map's zoom level or decreases it if the
+        // increment is negative.
+        increaseZoom: function (increment) {
+            var lvl = map.getZoom();
+            lvl = lvl + increment;
+            if (lvl < 1) {
+                lvl = 1;
+            }
+            map.setZoom(lvl);
+        },
+
+        // Sets the overlays available for this map
+        // { key: LayerConfig, ... }
+        setOverlays: function (overlaysToSet) {
+            overlays = overlaysToSet;
+        },
+
+        // Sets the baselayers available for this map
+        // { key: LayerConfig, ... }
+        setBaselayers: function (baselayersToSet) {
+            if (baselayersToSet) {
+                baselayers = baselayersToSet;
+            }
+        },
+
+        // Adds an overlay to the map
+        activateOverlay: function (key) {
+            var layerConfig = overlays[key];
+            if (layerConfig && layerConfig.type == 'wms') {
+                activeOverlays[key] = L.tileLayer.wms(layerConfig.url, layerConfig.layerOptions);
+                map.addLayer(activeOverlays[key]);
+            }
+        },
+
+        // Removes the old baselayer if neccessary and sets a new one
+        // identified by it's key
+        activateBaselayer: function (key) {
+            if (activeBaselayer) {
+                map.removeLayer(activeBaselayer);
+            }
+
+            var layerConfig = baselayers[key];
+            activeBaselayer = L.tileLayer(layerConfig.url, layerConfig.layerOptions);
+            activeBaselayerKey = key;
+            map.addLayer(activeBaselayer);
+        },
+
+        // Removes an overlay from the map
+        // TODO: Keep layer for later refresh
+        deactivateOverlay: function (key) {
+            map.removeLayer(activeOverlays[key]);
+            delete activeOverlays[key];
+        },
+
+        // Toggle an overlay on the map, identified by key
+        toggleOverlay: function (key) {
+            if (activeOverlays[key]) {
+                this.deactivateOverlay(key);
+            } else {
+                this.activateOverlay(key);
+            }
+
+        },
+
+        /**
+         * Set map view to center coords with zoomlevel
+         */
+        initializeView: function (lat, lng, zoom) {
+            var lt = lat || 40;
+            var lg = lng || -10;
+            var zm = zoom || 3;
+            map.setView([lt, lg], zm);
+        },
+
+        /**
+         * Returns a Query object copied from currentQuery and
+         * enriched with all parameters needed to recreate the current map
+         * @param stripExtraParams boolean
+         */
+        getMapQuery: function (query, stripExtraParams) {
+            return _getMapQuery(query, stripExtraParams);
+        }
+    }
+}]);
