e808be36a7fe7088139769ab1537201a7a6752c7
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-09-01 16:19:25 +0200

    Fix css for new map with recent grako pages.

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -2,7 +2,7 @@ var EC = protractor.ExpectedConditions;
 
 describe('con10t pages', function() {
 
-    it('should contain markers on the grako_map page', function() {
+    fit('should contain markers on the grako_map page', function() {
     	browser.get('/project/grako_map');
         var marker = element(by.css('.leaflet-marker-icon'));
         browser.wait(EC.presenceOf(marker), 10000);
