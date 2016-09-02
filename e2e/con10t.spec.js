bc9865a081dd47a55c412346a8a56ab6363c12e8
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-09-02 12:18:43 +0200

    Fix map test and put it to separate file.

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -1,16 +1,5 @@
 var EC = protractor.ExpectedConditions;
 
-describe('con10t pages', function() {
-
-    it('should contain markers on the grako_map page', function() {
-    	browser.get('/project/grako_map');
-        var marker = element(by.css('.leaflet-marker-icon'));
-        browser.wait(EC.presenceOf(marker), 10000);
-        expect(marker.isPresent()).toBe(true);
-    });
-
-});
-
 describe('con10t-image', function() {
 
     it('should display an image from a given url', function() {
