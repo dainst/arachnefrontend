3a4ae67b784aef96a7909c9fc3dfe04848357e67
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-09-06 18:39:29 +0200

    fix map e2e test

--- e2e/map.spec.js
+++ e2e/map.spec.js
@@ -13,7 +13,7 @@ describe('con10t pages', function() {
         browser.get('/project/grako_map');
 
         click().then(click).then(click).then(click).then(click).then(function() {
-            var marker = element(by.css('.leaflet-marker-icon'));
+            var marker = element(by.css('.awesome-marker'));
             browser.wait(EC.presenceOf(marker), 10000);
             expect(marker.isPresent()).toBe(true);
             done();
