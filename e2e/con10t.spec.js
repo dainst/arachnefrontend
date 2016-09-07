258689c6357b3a86556a7123e48b9cb39b5e7425
Daniel <danielmarreirosdeoliveira@googlemail.com>
2016-09-07 11:36:27 +0200

    Update con10t.spec.js

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -1,8 +1,8 @@
 var EC = protractor.ExpectedConditions;
 
-fdescribe('con10t-image', function() {
+describe('con10t-image', function() {
 
-    fit('should display an image from a given url', function() {
+    it('should display an image from a given url', function() {
         browser.get('/project/steindorff');
         var marker = element(by.css('.con10t-image-fromsrc'));
         browser.wait(EC.presenceOf(marker), 10000);
