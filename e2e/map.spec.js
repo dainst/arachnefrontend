545e70e191d35bd30932249462fa1bb015dc156f
Jan G. Wieners <jan@jan-wieners.de>
2016-09-20 12:32:17 +0200

    Fix test in dev environment.

--- e2e/map.spec.js
+++ e2e/map.spec.js
@@ -2,17 +2,15 @@ var EC = protractor.ExpectedConditions;
 
 describe('con10t page', function() {
 
-    it('should display a heatmap on the grako_map page', function(done) {
+    fit('should display a heatmap on the grako_map page', function(done) {
 
         browser.driver.manage().window().setSize(1280, 1024);
-
         browser.get('/project/grako_map');
-
-        browser.sleep(1000);
+        browser.waitForAngular();
 
         var heatmap = element(by.css('.leaflet-heatmap-layer'));
 
-        browser.wait(EC.presenceOf(heatmap), 5000);
+        browser.wait(EC.presenceOf(heatmap), 7000);
         expect(heatmap.isPresent()).toBe(true);
 
         done();
