8e781e2809288a01c92930ff0de2ff13a0c5fa33
Jan G. Wieners <jan@jan-wieners.de>
2016-09-20 08:55:50 +0200

    Fix test.

--- e2e/map.spec.js
+++ e2e/map.spec.js
@@ -9,19 +9,18 @@ describe('con10t pages', function() {
         return element.all(by.css('.leaflet-control-zoom-in')).get(0).getText().click();
     }
 
-    it('should contain markers on the grako_map page', function(done) {
+    fit('should contain markers on the grako_map page', function(done) {
 
+        browser.driver.manage().window().setSize(1920, 1600);
         browser.get('/project/grako_map');
 
-        click().then(click).then(click).then(click).then(function() {
+        click().then(click).then(click).then(click).then(click).then(click).then(click).then(function() {
 
             var marker = element(by.css('.awesome-marker'));
             browser.wait(EC.presenceOf(marker), 10000);
             expect(marker.isPresent()).toBe(true);
             done();
         })
-
-
     });
 
 });
