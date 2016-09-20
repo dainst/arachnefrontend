19d274b29293c1a4d6ab30c28b1b7355be7126fb
Jan G. Wieners <jan@jan-wieners.de>
2016-09-20 12:10:48 +0200

    Debug test.

--- e2e/map.spec.js
+++ e2e/map.spec.js
@@ -1,12 +1,14 @@
 var EC = protractor.ExpectedConditions;
 
-describe('con10t pages', function() {
+describe('con10t page', function() {
 
     it('should display a heatmap on the grako_map page', function(done) {
 
         browser.get('/project/grako_map');
 
         var heatmap = element(by.css('.leaflet-heatmap-layer'));
+
+        console.log(heatmap);
 
         browser.wait(EC.presenceOf(heatmap), 5000);
         expect(heatmap.isPresent()).toBe(true);
