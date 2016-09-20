99e97a0f2288b3f22a3d4b3518141da5375fa5df
Jan G. Wieners <jan@jan-wieners.de>
2016-09-20 12:14:58 +0200

    Fix test in dev environment.

--- e2e/map.spec.js
+++ e2e/map.spec.js
@@ -4,11 +4,13 @@ describe('con10t page', function() {
 
     it('should display a heatmap on the grako_map page', function(done) {
 
+        browser.driver.manage().window().setSize(1280, 1024);
+
         browser.get('/project/grako_map');
 
-        var heatmap = element(by.css('.leaflet-heatmap-layer'));
+        browser.sleep(1000);
 
-        console.log(heatmap);
+        var heatmap = element(by.css('.leaflet-heatmap-layer'));
 
         browser.wait(EC.presenceOf(heatmap), 5000);
         expect(heatmap.isPresent()).toBe(true);
