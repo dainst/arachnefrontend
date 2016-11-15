805338c6df74d9d348915e4cdf12227fe654ad74
shominees <jompa010@gmail.com>
2016-11-15 15:31:28 +0100

    raised entity display limit

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ angular.module('arachne',[
 }])
 .constant('arachneSettings', {
 	dataserviceUri: "//" + document.location.host + "/data",
-	limit: 20,
+	limit: 50,
 	facetLimit: 20,
 	openFacets : ["facet_image", "facet_kategorie", "facet_bestandsname", "facet_subkategoriebestand"],
 	sortableFields : ["entityId", "title", "subtitle"]
