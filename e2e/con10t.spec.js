a9c28f87d5a1758a33a06addfbb7dc979a98fd81
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-11-29 13:16:27 +0100

    add page object for con10timage (see #3823)

--- e2e/con10t.spec.js
+++ e2e/con10t.spec.js
@@ -1,20 +1,19 @@
 var EC = protractor.ExpectedConditions;
 
+var con10tImage = require('./con10t/con10t-image.page');
+
 describe('con10t-image', function() {
 
     it('should display an image from a given url', function() {
         browser.get('/project/steindorff');
-        var marker = element(by.css('.con10t-image-fromsrc'));
-        browser.wait(EC.presenceOf(marker), 10000);
-        expect(marker.isPresent()).toBe(true);
+        var image = con10tImage.getImageFromSrc();
+        expect(image.isPresent()).toBe(true);
     });
 
     it('should display an image from an arachne entity identifier', function() {
         browser.get('/project/schlossfulda');
-        var marker = element(by.css('.con10t-image-fromentityid'));
-        browser.wait(EC.presenceOf(marker), 10000);
-        expect(marker.isPresent()).toBe(true);
+        var image = con10tImage.getImageFromEntity();
+        expect(image.isPresent()).toBe(true);
     });
 
 });
-
---
+++ e2e/con10t/con10t-image.page.js
@@ -0,0 +1,15 @@
+var Con10tImagePage = function() {
+	
+    var imageFromSrc = element(by.css('.con10t-image-fromsrc'));
+    var imageFromEntity = element(by.css('.con10t-image-fromentityid'));
+
+    this.getImageFromSrc = function() {
+    	return imageFromSrc;
+    }
+
+    this.getImageFromEntity = function() {
+    	return imageFromEntity;
+    }
+}
+
+module.exports = new Con10tImagePage();
