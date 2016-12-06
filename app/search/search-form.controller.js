d39dbedd790911c995811e76077bde56c588ca04
shominees <jompa010@gmail.com>
2016-12-06 12:09:13 +0100

    added search suggestions

--- app/search/search-form.controller.js
+++ app/search/search-form.controller.js
@@ -2,16 +2,23 @@
 
 angular.module('arachne.controllers')
 
-    .controller('SearchFormController', ['$scope', '$location', function ($scope, $location) {
+    .controller('SearchFormController', ['$scope', '$location', 'arachneSettings', '$http', function ($scope, $location, arachneSettings, $http) {
 
-            $scope.search = function (fq) {
-                if ($scope.q) {
-                    var url = '/search?q=' + $scope.q;
-                    if (fq) url += "&fq=" + fq;
-                    $scope.q = null;
-                    $location.url(url);
-                }
+        $scope.search = function (fq) {
+            if ($scope.q) {
+                var url = '/search?q=' + $scope.q;
+                if (fq) url += "&fq=" + fq;
+                $scope.q = null;
+                $location.url(url);
             }
+        };
 
-        }
+        $scope.getSuggestions = function (value) {
+            console.log(value);
+            return $http.get(arachneSettings.dataserviceUri + '/suggest?q="' + value + '"')
+                .then(function (response) {
+                    return response.data.suggestions;
+                });
+        };
+    }
     ]);
