5e657101552db366c5d8700f720daa0c7ed17edd
Thomas Kleinke <thomas.kleinke@dainst.de>
2016-12-12 13:06:48 +0100

    removed misplaced delayPromises comments

--- e2e/basic-scenarios.spec.js
+++ e2e/basic-scenarios.spec.js
@@ -1,11 +1,3 @@
-/* 
- * In order to prevent errors caused by e2e tests running too fast you can slow them down by calling the following
- * function. Use higher values for slower tests.
- *
- * utils.delayPromises(50);
- *
- */
-
 var frontPage = require('./core/front.page');
 var navbar = require('./core/navbar.page');
 
