469c89fdf5b4d1634d63d87e52af0b7f22fc120c
paflov <philipp.franck@dainst.de>
2017-01-18 14:29:43 +0100

    bugfix: inproper hasCoodrinates-check filtered places on the equator or prime meridian

--- app/map/place.prototype.js
+++ app/map/place.prototype.js
@@ -24,8 +24,24 @@ angular.module('arachne.widgets.map')
             return this;
         },
 
-        hasCoordinates: function() {
-            return (this.location && this.location.lat && this.location.lon);
+		/**
+         * check properly if lat lon exists. if we would only check this.location && this.location.lat && this.location.lon
+         * you would exclude places on the prime meridian or equator
+         * also it would not return a boolean value.
+		 * @returns {boolean}
+		 */
+		hasCoordinates: function() {
+            return (
+                (typeof this.location !== "undefined") &&
+                (this.location !== null) &&
+                (this.location !== '') &&
+				(typeof this.location.lat !== "undefined") &&
+				(this.location.lat !== null) &&
+				(this.location.lat !== '') &&
+				(typeof this.location.lon !== "undefined") &&
+				(this.location.lon !== null) &&
+				(this.location.lon !== '')
+            )
         },
 
         getId: function() {
