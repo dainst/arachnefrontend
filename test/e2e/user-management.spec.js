ff4cf639fc08474957e51d3bf3b6c1aaaaacd1de
Simon Hohl <mail@dersmon.net>
2017-01-24 10:54:45 +0100

    Re-added duplicate user name test.

--- test/e2e/user-management.spec.js
+++ test/e2e/user-management.spec.js
@@ -313,30 +313,15 @@ describe('user management page', function () {
             })
     });
 
-    // it('registering with an existing username should cause "danger"-level message', function () {
-    //     common.createTestUserInDB();
-    //
-    //     navbarPage.clickRegistration()
-    //         .then(navbarPage.registrationTypeInUsername(common.getTestUserName()))
-    //         .then(navbarPage.registrationTypeInPassword(common.getTestUserPassword()))
-    //         .then(navbarPage.registrationTypeInPasswordValidation(common.getTestUserPassword()))
-    //         .then(navbarPage.registrationTypeInFirstname(common.getTestUserFirstname()))
-    //         .then(navbarPage.registrationTypeInLastname(common.getTestUserLastname()))
-    //         .then(navbarPage.registrationTypeInEmail(common.getTestUserEmail()))
-    //         .then(navbarPage.registrationTypeInEmailValidation(common.getTestUserEmail()))
-    //         .then(navbarPage.registrationTypeInInstitution(common.getTestUserInstitution()))
-    //         .then(navbarPage.registrationTypeInHomepage(common.getTestUserHomepage()))
-    //         .then(navbarPage.registrationTypeInZIP(common.getTestUserZIP()))
-    //         .then(navbarPage.registrationTypeInPlace(common.getTestUserCity()))
-    //         .then(navbarPage.registrationTypeInStreet(common.getTestUserStreet()))
-    //         .then(navbarPage.registrationSelectCountryByIndex(4))
-    //         .then(navbarPage.registrationTypeInPhone(common.getTestUserPhone()))
-    //         .then(navbarPage.registrationConfirmNoBot)
-    //         .then(navbarPage.submitRegistration)
-    //         .then(function () {
-    //             expect(messageBox.getLevel()).toEqual('danger');
-    //         })
-    // });
+    it('registering with an existing username should cause "danger"-level message', function () {
+        common.createTestUserInDB();
+
+        navbarPage.typeInCompleteRegistrationCredentials()
+            .then(navbarPage.submitRegistration)
+            .then(function () {
+                expect(messageBox.getLevel()).toEqual('danger');
+            })
+    });
 
     it('should be able to login and logout', function () {
         common.createTestUserInDB();
