cf797a6d5558df99836966d7ab1381afdc6b88d4
Jan G. Wieners <jan@jan-wieners.de>
2017-03-29 12:07:18 +0200

    Use the german version of grako_map.

--- test/e2e/map.spec.js
+++ test/e2e/map.spec.js
@@ -3,7 +3,7 @@ var map = require('./map/map.page');
 
 describe('map', function() {
     it('should include a heatmap on the grako_map page', function() {
-        browser.get('/project/grako_map');
+        browser.get('/project/grako_map?lang=de');
         var heatmap = map.getHeatmap();
         expect(heatmap.isPresent()).toBe(true);
     });
