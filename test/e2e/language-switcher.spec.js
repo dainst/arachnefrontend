c44bd1619db437d107e3d05f379689706edd4d57
Jan G. Wieners <jan@jan-wieners.de>
2017-03-29 11:45:46 +0200

    Add tests for language switcher.

---
+++ test/e2e/language-switcher.spec.js
@@ -0,0 +1,60 @@
+describe('language switcher', function() {
+
+    it('opens a submenu which lets users to choose between german and english', function() {
+
+        browser.get('/');
+
+        browser.wait(function() {
+            return element(by.id('language-switcher')).isPresent();
+        }, 1000);
+
+        var languageSwitcher = element(by.id('language-switcher'));
+
+        expect(languageSwitcher.isPresent()).toBe(true);
+
+        languageSwitcher.click();
+
+        expect(languageSwitcher.all(by.css('li')).get(0).getText()).toBe('Deutsch');
+        expect(languageSwitcher.all(by.css('li')).get(1).getText()).toBe('English');
+
+    });
+
+    it('changes the frontends language from german to english', function() {
+
+        browser.get('/');
+
+        browser.wait(function() {
+            return element(by.id('language-switcher')).isPresent();
+        }, 1000);
+
+        var languageSwitcher = element(by.id('language-switcher'));
+
+        expect(languageSwitcher.isPresent()).toBe(true);
+
+        languageSwitcher.click();
+
+        // Set frontends language to german
+        languageSwitcher.all(by.css('li')).get(0).click();
+
+        browser.wait(function() {
+            return element(by.id('language-switcher')).isPresent();
+        }, 1000);
+
+        expect(element.all(by.css('.navbar-links')).get(3).getText()).toBe('Über Arachne');
+        expect(element.all(by.css('.all-projects')).get(0).getText()).toBe('Alle Projekte anzeigen');
+
+        languageSwitcher.click();
+
+        // Set frontends language to english
+        languageSwitcher.all(by.css('li')).get(1).click();
+
+        browser.wait(function() {
+            return element(by.id('language-switcher')).isPresent();
+        }, 1000);
+
+        expect(element.all(by.css('.navbar-links')).get(3).getText()).toBe('About Arachne');
+        expect(element.all(by.css('.all-projects')).get(0).getText()).toBe('Show all projects');
+
+    });
+
+});
