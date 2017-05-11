9643e75ac5d5766c986610b0f31bd6833fa689fa
shominees <jompa010@gmail.com>
2017-05-11 12:16:53 +0200

    code refactoring for better efficiency

--- app/map/place.prototype.js
+++ app/map/place.prototype.js
@@ -4,64 +4,63 @@ angular.module('arachne.widgets.map')
 
 // Represents a place with entities
 // (as opposed to the entity with places, that is served by the backend)
-.factory('Place', function() {
+    .factory('Place', function () {
 
-    function Place() {
-        this.location = null; // { lon: 12.345, lat: 12.345 }
-        this.name = "";
-        this.gazetteerId = null;
-        this.entityCount = 0;
-        this.entities = [];
-        this.query = null; // A query to retrieve the entities in question
-    };
-
-    Place.prototype = {
-
-        merge: function(other) {
-            for (var key in other) {
-                this[key] = other[key];
-            }
-            return this;
-        },
-
-		/**
-         * check properly if lat lon exists. if we would only check this.location && this.location.lat && this.location.lon
-         * you would exclude places on the prime meridian or equator
-         * also it would not return a boolean value.
-		 * @returns {boolean}
-		 */
-		hasCoordinates: function() {
-            return (
-                (typeof this.location !== "undefined") &&
-                (this.location !== null) &&
-                (this.location !== '') &&
-				(typeof this.location.lat !== "undefined") &&
-				(this.location.lat !== null) &&
-				(this.location.lat !== '') &&
-				(typeof this.location.lon !== "undefined") &&
-				(this.location.lon !== null) &&
-				(this.location.lon !== '')
-            )
-        },
-
-        getId: function() {
-            if (this.hasCoordinates()) {
-                var id = this.location.lat + ',' + this.location.lon;
-            } else {
-                var id = this.name;
-            }
-            return id;
-        },
-
-        // adds an Entity to the place, this pushes the
-        addEntity: function(entity, relation) {
-            if (relation) {
-                entity.relation = relation;
-            }
-            this.entities.push(entity);
-            this.entityCount += 1;
+        function Place() {
+            this.location = null; // { lon: 12.345, lat: 12.345 }
+            this.name = "";
+            this.gazetteerId = null;
+            this.entityCount = 0;
+            this.entities = [];
+            this.query = null; // A query to retrieve the entities in question
         }
-    };
 
-    return Place;
-});
+        Place.prototype = {
+
+            merge: function (other) {
+                for (var key in other) {
+                    this[key] = other[key];
+                }
+                return this;
+            },
+
+            /**
+             * check properly if lat lon exists. if we would only check this.location && this.location.lat && this.location.lon
+             * you would exclude places on the prime meridian or equator
+             * also it would not return a boolean value.
+             * @returns {boolean}
+             */
+            hasCoordinates: function () {
+                return (
+                    (typeof this.location !== "undefined") &&
+                    (this.location !== null) &&
+                    (this.location !== '') &&
+                    (typeof this.location.lat !== "undefined") &&
+                    (this.location.lat !== null) &&
+                    (this.location.lat !== '') &&
+                    (typeof this.location.lon !== "undefined") &&
+                    (this.location.lon !== null) &&
+                    (this.location.lon !== '')
+                )
+            },
+
+            getId: function () {
+                var id = this.name;
+                if (this.hasCoordinates()) {
+                    id = this.location.lat + ',' + this.location.lon;
+                }
+                return id;
+            },
+
+            // adds an Entity to the place
+            addEntity: function (entity, relation) {
+                if (relation) {
+                    entity.relation = relation;
+                }
+                this.entities.push(entity);
+                this.entityCount += 1;
+            }
+        };
+
+        return Place;
+    });
--- app/map/places.service.js
+++ app/map/places.service.js
@@ -2,104 +2,104 @@
 
 angular.module('arachne.widgets.map')
 
-/*
+/**
  * Provides a list of Place objects corresponding to the entities in the current search result
  *
  * @author: David Neugebauer
  */
-.factory('placesService', ['searchService', 'Place', function(searchService, Place) {
+    .factory('placesService', ['searchService', 'Place', function (searchService, Place) {
 
-    // A promise for an Array of Place objects with corresponding entities
-    var promise = null;
+        // A promise for an Array of Place objects with corresponding entities
+        var promise = null;
 
-    // Keep a count of all entities that have connected places
-    var entityCount = 0;
+        // Keep a count of all entities that have connected places
+        var entityCount = 0;
 
-    // reads a list of entities with connected places
-    // returns a list of places with connected entities
-    var buildPlacesListFromEntityList = function(entities,bbox) {
+        // reads a list of entities with connected places
+        // returns a list of places with connected entities
+        var buildPlacesListFromEntityList = function (entities, bbox) {
 
-        var places = {};
-        var placesList = [];
-        entityCount = 0;
+            var places = {};
+            var placesList = [];
+            entityCount = 0;
 
-        for(var i = 0; i < entities.length; i++) {
-            var entity = entities[i];
+            for (var i = 0; i < entities.length; i++) {
+                var entity = entities[i];
 
-            if (entity.places) {
-                entityCount += 1;
+                if (entity.places) {
+                    entityCount += 1;
 
-                for (var j = 0; j < entity.places.length; j++) {
-                    var place = new Place();
-                    place.merge(entity.places[j]);
+                    for (var j = 0; j < entity.places.length; j++) {
+                        var place = new Place();
+                        place.merge(entity.places[j]);
 
-                    if ( bbox && place.location && place.location.lat && place.location.lon && (
-                            parseFloat(bbox[0])>parseFloat(place.location.lat) &&
-                            parseFloat(place.location.lat)>parseFloat(bbox[2]) &&
-                            parseFloat(bbox[1])<parseFloat(place.location.lon) &&
-                            parseFloat(place.location.lon) < parseFloat(bbox[3])
-                        )) {
+                        if (bbox && place.location && place.location.lat && place.location.lon && (
+                                parseFloat(bbox[0]) > parseFloat(place.location.lat) &&
+                                parseFloat(place.location.lat) > parseFloat(bbox[2]) &&
+                                parseFloat(bbox[1]) < parseFloat(place.location.lon) &&
+                                parseFloat(place.location.lon) < parseFloat(bbox[3])
+                            )) {
 
-                        // If there already is a place with the same id, use
-                        // that one instead and just add the entity
-                        var key = place.getId();
-                        if (places[key]) {
-                            places[key].addEntity(entity, place.relation);
-                        } else {
-                            place.addEntity(entity, place.relation);
-                            places[key] = place;
+                            // If there already is a place with the same id, use
+                            // that one instead and just add the entity
+                            var key = place.getId();
+                            if (places[key]) {
+                                places[key].addEntity(entity, place.relation);
+                            } else {
+                                place.addEntity(entity, place.relation);
+                                places[key] = place;
+                            }
+
+                            if (!place.query && place.gazetteerId) {
+                                place.query = searchService.currentQuery().removeParams(['fl', 'lat', 'lng', 'zoom', 'overlays']);
+                                place.query.q = place.query.q + " places.gazetteerId:" + place.gazetteerId;
+                            }
+
                         }
-
-                        if (!place.query && place.gazetteerId) {
-                            place.query = searchService.currentQuery().removeParams(['fl', 'lat', 'lng', 'zoom', 'overlays']);
-                            place.query.q = place.query.q + " places.gazetteerId:" + place.gazetteerId;
-                        }
-
-                    } 
+                    }
                 }
             }
-        }
 
-        for (key in places) {
-            placesList.push(places[key]);
-        }
-
-        return placesList;
-    };
-
-    var inBbox = function(bbox, loc) {
-        return bbox[0] > loc.lat && bbox[1] < loc.lon && bbox[2] < loc.lat && bbox[3] > loc.lon;
-    };
-
-    var buildPlacesFromFacet = function(facet, bbox) {
-        var places = [];
-        facet.values.forEach(function(value) {
-            var place = new Place().merge(JSON.parse(value.value));
-            if (place.location) {
-                place.query = searchService.currentQuery().removeParams(
-                    ['fl', 'lat', 'lng', 'zoom', 'overlays','baselayer']);
-                if (place.query.q == '*') place.query.q = "places.gazetteerId:" + place.gazetteerId;
-                else place.query.q = place.query.q + " AND places.gazetteerId:" + place.gazetteerId;
-                places.push(place);
+            for (key in places) {
+                placesList.push(places[key]);
             }
-        });
-        return places;
-    }
 
-    return {
+            return placesList;
+        };
 
-        makePlacesFromFacet: function(facet, bbox) {
-            return buildPlacesFromFacet(facet, bbox);
-        },
+        var inBbox = function (bbox, loc) {
+            return bbox[0] > loc.lat && bbox[1] < loc.lon && bbox[2] < loc.lat && bbox[3] > loc.lon;
+        };
 
-        makePlacesFromEntities: function(entities,bbox) {
-            return buildPlacesListFromEntityList(entities,bbox);
-        },
+        var buildPlacesFromFacet = function (facet, bbox) {
+            var places = [];
+            facet.values.forEach(function (value) {
+                var place = new Place().merge(JSON.parse(value.value));
+                if (place.location) {
+                    place.query = searchService.currentQuery().removeParams(
+                        ['fl', 'lat', 'lng', 'zoom', 'overlays', 'baselayer']);
+                    if (place.query.q === '*') place.query.q = "places.gazetteerId:" + place.gazetteerId;
+                    else place.query.q = place.query.q + " AND places.gazetteerId:" + place.gazetteerId;
+                    places.push(place);
+                }
+            });
+            return places;
+        };
 
-        // the number of entities in the current search
-        // that are connected to places
-        getEntityCount: function() {
-            return entityCount;
+        return {
+
+            makePlacesFromFacet: function (facet, bbox) {
+                return buildPlacesFromFacet(facet, bbox);
+            },
+
+            makePlacesFromEntities: function (entities, bbox) {
+                return buildPlacesListFromEntityList(entities, bbox);
+            },
+
+            // the number of entities in the current search
+            // that are connected to places
+            getEntityCount: function () {
+                return entityCount;
+            }
         }
-    }
-}]);
+    }]);
