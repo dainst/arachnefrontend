3fd4e57871c23871e0433e7e6e4b9db8824e5300
Jan G. Wieners <jan@jan-wieners.de>
2017-07-11 16:33:29 +0200

    Rewrite tile handling to fit leaflet's 1.x api.

--- app/image/zoomifyimg.directive.js
+++ app/image/zoomifyimg.directive.js
@@ -6,43 +6,60 @@ angular.module('arachne.directives')
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
+
+
                 /*
                  * L.TileLayer.Zoomify display Zoomify tiles with Leaflet
+                 *
+                 * Developed for Leaflet 1.0-dev
+                 *
+                 * Based on the Leaflet.Zoomify (https://github.com/turban/Leaflet.Zoomify)
+                 * from turban (https://github.com/turban)
+                 *
                  */
-                L.TileLayer.Zoomify = L.TileLayer.extend({
 
+                L.TileLayer.Zoomify = L.TileLayer.extend({
                     options: {
                         continuousWorld: true,
                         tolerance: 0.8
                     },
-
                     initialize: function (entityId, options) {
-                        options = L.setOptions(this, options);
+
+                        var options = L.setOptions(this, options);
                         this._entityId = entityId;
 
                         var imageSize = L.point(options.width, options.height),
                             tileSize = options.tileSize;
 
+                        //Build the zoom sizes of the pyramid and cache them in an array
                         this._imageSize = [imageSize];
                         this._gridSize = [this._getGridSize(imageSize)];
 
+                        //Register the image size in pixels and the grid size in # of tiles for each zoom level
                         while (parseInt(imageSize.x) > tileSize || parseInt(imageSize.y) > tileSize) {
-                            imageSize = imageSize.divideBy(2).floor();
+                            imageSize = imageSize.divideBy(2).ceil();
                             this._imageSize.push(imageSize);
                             this._gridSize.push(this._getGridSize(imageSize));
                         }
 
+                        //We built the cache from bottom to top, but leaflet uses a top to bottom index for the zoomlevel,
+                        // so reverse it for easy indexing by current zoomlevel
                         this._imageSize.reverse();
                         this._gridSize.reverse();
 
-                        this.options.maxZoom = this._gridSize.length - 1;
-                        var southWest = map.unproject([0, options.height], this.options.maxZoom);
-                        var northEast = map.unproject([options.width, 0], this.options.maxZoom);
-                        map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
+                        //Register our max supported zoom level
+                        options.maxNativeZoom = this._gridSize.length - 1;
 
+                        //Register our bounds for this zoomify layer based on the maximum zoom
+                        var southWest = map.unproject([0, options.height], options.maxNativeZoom);
+                        var northEast = map.unproject([options.width, 0], options.maxNativeZoom);
+
+                        options.bounds = new L.LatLngBounds(southWest, northEast);
+                        map.setMaxBounds(options.bounds);
                     },
 
                     onAdd: function (map) {
+
                         L.TileLayer.prototype.onAdd.call(this, map);
                         var mapSize = map.getSize(),
                             zoom = this._getBestFitZoom(mapSize),
@@ -52,15 +69,18 @@ angular.module('arachne.directives')
                         map.setView(center, zoom, true);
                     },
 
+                    //Calculate the grid size for a given image size (based on tile size)
                     _getGridSize: function (imageSize) {
+
                         var tileSize = this.options.tileSize;
                         return L.point(Math.ceil(imageSize.x / tileSize), Math.ceil(imageSize.y / tileSize));
                     },
 
                     _getBestFitZoom: function (mapSize) {
+
                         var tolerance = this.options.tolerance,
                             zoom = this._imageSize.length - 1,
-                            imageSize, zoom;
+                            imageSize;
 
                         while (zoom) {
                             imageSize = this._imageSize[zoom];
@@ -74,17 +94,24 @@ angular.module('arachne.directives')
                     },
 
                     _tileShouldBeLoaded: function (tilePoint) {
+
                         var gridSize = this._gridSize[this._map.getZoom()];
                         return (tilePoint.x >= 0 && tilePoint.x < gridSize.x && tilePoint.y >= 0 && tilePoint.y < gridSize.y);
                     },
 
+                    //Extend the add tile function to update our arbitrary sized border tiles
                     _addTile: function (tilePoint, container) {
+
                         var tilePos = this._getTilePos(tilePoint),
-                            tile = this._getTile(),
-                            zoom = this._map.getZoom(),
-                            imageSize = this._imageSize[zoom],
-                            gridSize = this._gridSize[zoom],
+                            key = this._tileCoordsToKey(tilePoint),
+                            imageSize = this._imageSize[this._getZoomForUrl()],
+                            gridSize = this._gridSize[this._getZoomForUrl()],
                             tileSize = this.options.tileSize;
+
+                        //Load the tile via the original leaflet code
+                        L.TileLayer.prototype._addTile.call(this, tilePoint, container);
+                        var tile = this._tiles[key].el,
+                            zoom = this._map.getZoom();
 
                         if (tilePoint.x === gridSize.x - 1) {
                             tile.style.width = imageSize.x - (tileSize * (gridSize.x - 1)) + 'px';
@@ -96,26 +123,52 @@ angular.module('arachne.directives')
 
                         L.DomUtil.setPosition(tile, tilePos, L.Browser.chrome || L.Browser.android23);
 
-                        this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
+                        //this._tiles[tilePoint.x + ':' + tilePoint.y] = tile;
                         this._loadTile(tile, tilePoint);
 
                         if (tile.parentNode !== this._tileContainer) {
                             container.appendChild(tile);
                         }
+
+                        return true;
+
+                        //Get out imagesize in pixels for this zoom level and our grid size
+                        var imageSize = this._imageSize[this._getZoomForUrl()],
+                            gridSize = this._gridSize[this._getZoomForUrl()];
+
+                        //The real tile size (default:256) and the display tile size (if zoom > maxNativeZoom)
+                        var	realTileSize = L.GridLayer.prototype.getTileSize.call(this),
+                            displayTileSize = L.TileLayer.prototype.getTileSize.call(this);
+
+                        //Get the current tile to adjust
+                        var key = this._tileCoordsToKey(coords),
+                            tile = this._tiles[key].el;
+
+                        //Calculate the required size of the border tiles
+                        var scaleFactor = L.point(	(imageSize.x % realTileSize.x),
+                            (imageSize.y % realTileSize.y)).unscaleBy(realTileSize);
+
+                        //Update tile dimensions if we are on a border
+                        if ((imageSize.x % realTileSize.x) > 0 && coords.x === gridSize.x - 1) {
+                            tile.style.width = displayTileSize.scaleBy(scaleFactor).x + 'px';
+                        }
+
+                        if ((imageSize.y % realTileSize.y) > 0 && coords.y === gridSize.y - 1) {
+                            tile.style.height = displayTileSize.scaleBy(scaleFactor).y + 'px';
+                        }
+
+                        this._loadTile(tile, coords);
                     },
 
                     // override to use XHR instead of regular image loading
                     _loadTile: function (tile, tilePoint) {
-                        tile._layer = this;
-                        tile.onload = this._tileOnLoad;
-                        tile.onerror = this._tileOnError;
 
-                        this._adjustTilePoint(tilePoint);
+                        tile._layer = this;
+
+                        //this._adjustTilePoint(tilePoint);
                         var imgUri = this.getTileUrl(tilePoint);
                         $http.get(imgUri, {responseType: 'arraybuffer'})
-                            .then(function (result) {
-
-                                var data = result.data;
+                            .success(function (data) {
                                     var blob = new Blob([data], {type: 'image/jpeg'});
                                     tile.src = window.URL.createObjectURL(blob);
                                 }
@@ -127,30 +180,20 @@ angular.module('arachne.directives')
                         });
                     },
 
-                    getTileUrl: function (tilePoint) {
-                        return arachneSettings.dataserviceUri + '/image/zoomify/' + this._entityId + '/' + this._map.getZoom() + '-' + tilePoint.x + '-' + tilePoint.y + '.jpg';
-                    },
-
-                    _getTileGroup: function (tilePoint) {
-                        var zoom = this._map.getZoom(),
-                            num = 0,
-                            gridSize;
-
-                        for (var z = 0; z < zoom; z++) {
-                            gridSize = this._gridSize[z];
-                            num += gridSize.x * gridSize.y;
-                        }
-
-                        num += tilePoint.y * this._gridSize[zoom].x + tilePoint.x;
-                        return Math.floor(num / 256);
+                    getTileUrl: function (coords) {
+                        return arachneSettings.dataserviceUri + '/image/zoomify/' + this._entityId + '/' + this._map.getZoom() + '-' + coords.x + '-' + coords.y + '.jpg';
                     }
-
                 });
+
+                L.tileLayer.zoomify = function (url, options) {
+                    return new L.TileLayer.Zoomify(url, options);
+                };
 
                 L.tileLayer.zoomify = function (entityId, options) {
                     return new L.TileLayer.Zoomify(entityId, options);
                 };
                 var map = L.map(element[0]).setView([0, 0], 0);
+
                 L.tileLayer.zoomify(attrs.entityid, {
                     width: scope.imageProperties.width,
                     height: scope.imageProperties.height,
