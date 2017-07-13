33b3a238dd5bffd9d9e396a77e710e8de6b9c376
shominees <jompa010@gmail.com>
2017-07-13 13:18:38 +0200

    added experimental dual row design for con10t-media-tree

--- app/utils/con10t-media-tree.directive.js
+++ app/utils/con10t-media-tree.directive.js
@@ -20,6 +20,17 @@ angular.module('arachne.widgets.directives')
                 scope.arachneUrl = arachneSettings.arachneUrl;
                 scope.cols = 12 / scope.images.length;
                 scope.hasTeaser = attrs.headerTeaser;
+                scope.firstBatch = scope.images;
+
+                // experimental 2 row design, only with even amount of images
+                if (scope.images.length > 3) {
+                    var midLength = scope.images.length / 2;
+                    scope.firstBatch = scope.images.slice(0, midLength);
+                    scope.secondBatch = scope.images.slice(midLength, scope.images.length);
+                    scope.cols = 12 / midLength;
+                }
+
+                // 3 row design? 3 images per row?
 
                 $transclude(function (clone) {
                     scope.showCaption = clone.length;
