cb874c20228f596f37ef9dc7da29cb4d209afd56
Simon Hohl <simon.hohl@dainst.org>
2017-10-17 11:56:53 +0200

    Add viewer for SVG data
    Closes Redmine-Tickets #6606 and #6606.

--- app/app.js
+++ app/app.js
@@ -87,6 +87,7 @@ angular.module('arachne',[
 
 			'gridmap':			{ url: '/gridmap', templateUrl: 'app/map/gridmap.html', data: { pageTitle: title }},
 			'3d':				{ url: '/3d', templateUrl: 'app/3d/3d.html', data: { pageTitle: title }},
+			'SVG':				{ url: '/SVG', templateUrl: 'app/SVG/svg.html', data: { pageTitle: title }},
 			'projects':			{ url: '/projects', templateUrl: 'app/pages/projects.html', data: { pageTitle: title }},
 			'register':			{ url: '/register', templateUrl: 'app/users/register.html', data: { pageTitle: title }},
 			'editUser':			{ url: '/editUser', templateUrl: 'app/users/edit-user.html', data: { pageTitle: title }},
