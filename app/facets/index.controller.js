81b1492498c33a20fafe2639ffa41ff8f7a04695
shominees <jompa010@gmail.com>
2017-11-10 13:00:01 +0100

    more comments

--- app/facets/index.controller.js
+++ app/facets/index.controller.js
@@ -1,5 +1,14 @@
 angular.module('arachne.controllers')
-    .controller('IndexController', ['$rootScope', '$scope', '$sce', 'categoryService', 'Entity', 'Query', '$stateParams', '$http','$filter', '$location',
+/**
+ * Allows browsing in facets and dependent facet values
+ *
+ * controls the display of facets and facet values
+ * based on dependent selections saved as URL params
+ *
+ * @author
+ * @author Patrick Jominet
+ */
+    .controller('IndexController', ['$rootScope', '$scope', '$sce', 'categoryService', 'Entity', 'Query', '$stateParams', '$http', '$filter', '$location',
         function ($rootScope, $scope, $sce, categoryService, Entity, Query, $stateParams, $http, $filter, $location) {
             $scope.currentCategory = undefined;
             $scope.currentFacet = undefined;
@@ -10,28 +19,32 @@ angular.module('arachne.controllers')
             $scope.minPanelSize = 14;
             $scope.panelSize = $scope.minPanelSize;
 
-            $scope.previousFacetPage = function() {
+            $scope.previousFacetPage = function () {
                 $scope.currentFacetPage -= 1;
             };
 
-            $scope.nextFacetPage = function() {
+            $scope.nextFacetPage = function () {
                 $scope.currentFacetPage += 1;
             };
 
-            $scope.previousValuePage = function() {
+            $scope.previousValuePage = function () {
                 $scope.currentValuePage -= 1;
             };
 
-            $scope.nextValuePage = function() {
+            $scope.nextValuePage = function () {
                 $scope.currentValuePage += 1;
             };
 
+            /**
+             * Reload view if URL gets changed (additional params added)
+             */
             $rootScope.$on('$locationChangeSuccess', function () {
+                console.log($stateParams);
                 load();
             });
 
             /**
-             * load facets and facet values combined
+             * Load facets and facet values combined
              */
             function load() {
                 loadFacets();
@@ -41,6 +54,7 @@ angular.module('arachne.controllers')
 
             /**
              * Build a query from facets and/or facet values
+             * to be used in a search request
              *
              * @return query object with all selected facets and facet values and '*' as query string
              */
@@ -48,12 +62,12 @@ angular.module('arachne.controllers')
                 var query = new Query();
 
                 // the selected facet
-                if($stateParams.c){
+                if ($stateParams.c) {
                     query = query.addFacet("facet_kategorie", $stateParams.c)
                 }
 
                 // the selected facet value
-                if($stateParams.fq && $stateParams.fv){
+                if ($stateParams.fq && $stateParams.fv) {
                     query = query.addFacet($stateParams.fq, $stateParams.fv)
                 }
 
@@ -65,7 +79,7 @@ angular.module('arachne.controllers')
              * make a search request with the selected facet values from current query
              * change location in doing so
              */
-            $scope.startIndexSearch = function() {
+            $scope.startIndexSearch = function () {
                 $location.url("search" + getCurrentQuery().toString());
             };
 
@@ -80,21 +94,22 @@ angular.module('arachne.controllers')
 
             /**
              * Load categories and sort them alphabetically
+             * categoryService loads category data from json
              */
-        	categoryService.getCategoriesAsync().then(function (categories) {
+            categoryService.getCategoriesAsync().then(function (categories) {
 
-        	    var temp = [];
+                var temp = [];
                 for (var key in categories) {
                     if (categories[key].status !== 'none') {
                         temp.push(categories[key]);
                     }
                 }
 
-                if(temp.length >= $scope.minPanelSize) $scope.panelSize = temp.length;
+                if (temp.length >= $scope.minPanelSize) $scope.panelSize = temp.length;
 
                 $scope.categories = temp.sort(function (a, b) {
-                    if(a.title < b.title) return -1;
-                    else if(a.title > b.title) return 1;
+                    if (a.title < b.title) return -1;
+                    else if (a.title > b.title) return 1;
                     else return 0;
                 });
             });
@@ -115,12 +130,14 @@ angular.module('arachne.controllers')
 
                     Entity.query($scope.currentCategoryQuery.toFlatObject(), function (response) {
                         // get all facets except facet_geo
-                        var filteredFacets = response.facets.filter(function(facet){ return facet.name !== "facet_geo"});
+                        var filteredFacets = response.facets.filter(function (facet) {
+                            return facet.name !== "facet_geo"
+                        });
 
                         // sort facets alphabetically
                         filteredFacets = filteredFacets.sort(function (a, b) {
-                            if($filter('transl8')(a.name).toLowerCase() < $filter('transl8')(b.name).toLowerCase()) return -1;
-                            if($filter('transl8')(a.name).toLowerCase() > $filter('transl8')(b.name).toLowerCase()) return 1;
+                            if ($filter('transl8')(a.name).toLowerCase() < $filter('transl8')(b.name).toLowerCase()) return -1;
+                            if ($filter('transl8')(a.name).toLowerCase() > $filter('transl8')(b.name).toLowerCase()) return 1;
                             return 0;
                         });
 
@@ -131,16 +148,16 @@ angular.module('arachne.controllers')
                         $scope.facetCount = filteredFacets.length;
 
                         $scope.currentFacetPage = 0;
-                        for(var i = 0; i < filteredFacets.length; i++) {
+                        for (var i = 0; i < filteredFacets.length; i++) {
                             // determine number of pages and add pages to array
-                            if(itemsPerPage === $scope.panelSize) {
+                            if (itemsPerPage === $scope.panelSize) {
                                 $scope.facets.push([]);
                                 pageCounter += 1;
                                 itemsPerPage = 0;
                             }
 
                             // Page selection based on selected facet?
-                            if(filteredFacets[i].name === $scope.currentFacet){
+                            if (filteredFacets[i].name === $scope.currentFacet) {
                                 $scope.currentFacetPage = pageCounter;
                             }
 
@@ -157,19 +174,19 @@ angular.module('arachne.controllers')
             }
 
             /**
-             * Load facets values for a selected facet from url param 'fq'
+             * Load facets values for a selected facet from url param 'fq', 'fv' and 'group'
              */
             function loadFacetValues() {
                 if ($stateParams.fq) {
                     // no selection changes
                     if ($scope.currentFacet === $stateParams.fq
-                            && $scope.currentValue === $stateParams.fv
-                            && $scope.groupedBy === $stateParams.group
-                            && $scope.facetValues){
+                        && $scope.currentValue === $stateParams.fv
+                        && $scope.groupedBy === $stateParams.group
+                        && $scope.facetValues) {
                         return;
                     }
 
-                    if($scope.groupedBy !== $stateParams.group){
+                    if ($scope.groupedBy !== $stateParams.group) {
                         $scope.currentValuePage = 0;
                     }
 
@@ -185,19 +202,21 @@ angular.module('arachne.controllers')
 
                     $http.get(url).then(function (result) {
                         // get only non empty values
-                        var preprocessedValues = result.data.facetValues.filter( function(value){ return value.trim() !== ""});
+                        var preprocessedValues = result.data.facetValues.filter(function (value) {
+                            return value.trim() !== ""
+                        });
                         // trim all remaining whitespace from facet values
-                        preprocessedValues = preprocessedValues.map(function(value) {
+                        preprocessedValues = preprocessedValues.map(function (value) {
                             return value.trim();
                         });
                         // Filtering duplicates
-                        var temp = preprocessedValues.filter(function(value, index, self){
+                        var temp = preprocessedValues.filter(function (value, index, self) {
                             return index === self.indexOf(value);
                         });
                         // sort facet values alphabetically
                         preprocessedValues = temp.sort(function (a, b) {
-                            if(a.toLowerCase() < b.toLowerCase()) return -1;
-                            if(a.toLowerCase() > b.toLowerCase()) return 1;
+                            if (a.toLowerCase() < b.toLowerCase()) return -1;
+                            if (a.toLowerCase() > b.toLowerCase()) return 1;
                             return 0;
                         });
 
@@ -208,15 +227,15 @@ angular.module('arachne.controllers')
                         $scope.valuesCount = preprocessedValues.length;
 
                         // ?
-                        if(preprocessedValues.length + 2 < $scope.panelSize)
+                        if (preprocessedValues.length + 2 < $scope.panelSize)
                             $scope.valueRows = 1;
                         else $scope.valueRows = 2;
 
                         // ???
-                        if($scope.facets !== undefined) {
+                        if ($scope.facets !== undefined) {
                             var currentIndex = 0;
                             for (var i = 0; i < $scope.facets[0].length; i++) {
-                                if($scope.facets[0][i].name === $scope.currentFacet) {
+                                if ($scope.facets[0][i].name === $scope.currentFacet) {
                                     currentIndex = i; //This will be needed at thursday, 14.02.2017 ???
                                     break;
                                 }
@@ -224,16 +243,16 @@ angular.module('arachne.controllers')
                         }
 
                         $scope.currentValuePage = 0;
-                        for(var i = 0; i < preprocessedValues.length; i++) {
+                        for (var i = 0; i < preprocessedValues.length; i++) {
                             // determine number of pages and add pages to array
-                            if(itemsPerPage + 2 === $scope.panelSize * 2) {
+                            if (itemsPerPage + 2 === $scope.panelSize * 2) {
                                 $scope.facetValues.push([]);
                                 pageCounter += 1;
                                 itemsPerPage = 0;
                             }
 
                             // Page selection based on selected facet value?
-                            if(preprocessedValues[i] === $scope.currentValue){
+                            if (preprocessedValues[i] === $scope.currentValue) {
                                 $scope.currentValuePage = pageCounter;
                             }
 
