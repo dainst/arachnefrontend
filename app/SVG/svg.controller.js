f354e86bfe490f9189ba103d9013fe27341989ae
Simon Hohl <simon.hohl@dainst.org>
2017-11-22 13:23:50 +0100

    Move SVG navigation and metadata to overlay
    Closes Redmine ticket #6644.

--- app/SVG/svg.controller.js
+++ app/SVG/svg.controller.js
@@ -9,21 +9,24 @@ angular.module('arachne.controllers')
     .controller('SvgController', ['$scope', '$http', '$location', 'arachneSettings',
         function ($scope, $http, $location, arachneSettings) {
 
-            $scope.titleDisplay = document.querySelector("#svg-title");
-            $scope.licenseDisplay = document.querySelector("#svg-license");
-            $scope.modellerDisplay = document.querySelector("#svg-modeller");
-
+            $scope.titleDisplay = document.querySelector('#svg-title');
+            $scope.licenseDisplay = document.querySelector('#svg-license');
+            $scope.modellerDisplay = document.querySelector('#svg-modeller');
+            $scope.statusElement = document.querySelector('#svg-status');
             $scope.requestedId = null;
             $scope.panZoomObject = null;
 
             $scope.init = function (){
                 $scope.requestedId = $location.search().id;
 
-                var metadataRequestURL = arachneSettings.dataserviceUri + '/model/' + $scope.requestedId  + "?meta=true";
+                var metadataRequestURL = arachneSettings.dataserviceUri + '/model/' + $scope.requestedId  + '?meta=true';
                 $http.get(metadataRequestURL)
                     .then(function(response) {
                         if(!$scope.validFormat(response.data.format)){
-                            console.dir(response.data.format + "is no valid vector graphics format for this viewer.");
+                            var msg = 'Error: "' + response.data.format + '" is no valid vector graphics format for this viewer.';
+                            $scope.statusElement.innerHTML = msg;
+                            $scope.statusElement.classList.remove('alert-success');
+                            $scope.statusElement.classList.add('alert-danger');
                             return;
                         }
                         $scope.displayMetadata(response.data);
@@ -51,10 +54,15 @@ angular.module('arachne.controllers')
 
             $scope.loadSVGData = function(url) {
                 $http.get(url).then(function (response) {
-                    var svgContainer = document.querySelector('#svg-container');
-                    svgContainer.innerHTML = response.data;
+                    var svgContainer = document.querySelector('#svg-data-container');
 
-                    var svgContent = document.querySelector('svg');
+                    svgContainer.innerHTML = response.data;
+                    var metadataElement = document.querySelector('#svg-metadata-container');
+
+                    $scope.statusElement.style.display = 'none';
+                    metadataElement.style.display = 'block';
+
+                    var svgContent = svgContainer.querySelector('svg');
 
                     $scope.panZoomObject = svgPanZoom(svgContent, {
                         controlIconsEnabled: false,
@@ -66,6 +74,18 @@ angular.module('arachne.controllers')
                 }, function (error) {
                     console.dir(error);
                 })
+            };
+
+            $scope.zoomInSVG = function () {
+                $scope.panZoomObject.zoomIn();
+            };
+
+            $scope.zoomOutSVG = function () {
+                $scope.panZoomObject.zoomOut();
+            };
+
+            $scope.resetSVG = function () {
+                $scope.panZoomObject.reset();
             };
 
             $scope.init();
