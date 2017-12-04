07cd1ea5453a7002871c8b98a66e89b706c6eeb8
shominees <jompa010@gmail.com>
2017-12-04 12:54:35 +0100

    con10t-image tests now use widgetsdemo as test page

--- test/e2e/con10t.spec.js
+++ test/e2e/con10t.spec.js
@@ -4,14 +4,14 @@ var con10tImage = require('./con10t/con10t-image.page');
 
 describe('con10t-image', function() {
 
-    it('should display an image from a given url', function() {
-        browser.get('/project/steindorff');
+    fit('should display an image from a given url', function() {
+        browser.get('/project/widgetsdemo');
         var image = con10tImage.getImageFromSrc();
         expect(image.isPresent()).toBe(true);
     });
 
-    it('should display an image from an arachne entity identifier', function() {
-        browser.get('/project/schlossfulda');
+    fit('should display an image from an arachne entity identifier', function() {
+        browser.get('/project/widgetsdemo');
         var image = con10tImage.getImageFromEntity();
         expect(image.isPresent()).toBe(true);
     });
