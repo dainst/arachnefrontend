2e2c52122c674b6cc6d31a7bccfaeaa021518a05
shominees <jompa010@gmail.com>
2017-12-04 13:22:36 +0100

    changed back from widgetsdemo, because it produces errors

--- test/e2e/con10t.spec.js
+++ test/e2e/con10t.spec.js
@@ -4,13 +4,14 @@ var con10tImage = require('./con10t/con10t-image.page');
 
 describe('con10t-image', function() {
 
-    fit('should display an image from a given url', function() {
-        browser.get('/project/widgetsdemo');
+    it('should display an image from a given url', function() {
+        browser.get('/project/fotothekmuenchen');
         var image = con10tImage.getImageFromSrc();
         expect(image.isPresent()).toBe(true);
     });
 
-    fit('should display an image from an arachne entity identifier', function() {
+    // TODO: schlossfulda or widgetsdemo url do not work, wait for testproject
+    xit('should display an image from an arachne entity identifier', function() {
         browser.get('/project/widgetsdemo');
         var image = con10tImage.getImageFromEntity();
         expect(image.isPresent()).toBe(true);
