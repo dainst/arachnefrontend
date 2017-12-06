be285c4a39148ceb12b24be6c273174924732e1a
shominees <jompa010@gmail.com>
2017-12-06 10:21:14 +0100

    removed fit from tests

--- test/e2e/con10t.spec.js
+++ test/e2e/con10t.spec.js
@@ -4,13 +4,13 @@ var con10tWidget = require('./con10t/con10t-widget.page');
 
 describe('con10t-image', function() {
 
-    fit('should display an image from a given url', function() {
+    it('should display an image from a given url', function() {
         browser.get('/project/test_project');
         var image = con10tWidget.getImageFromSrc();
         expect(image.isPresent()).toBe(true);
     });
 
-    fit('should display an image from an arachne entity identifier', function() {
+    it('should display an image from an arachne entity identifier', function() {
         browser.get('/project/test_project');
         var image = con10tWidget.getImageFromEntity();
         expect(image.isPresent()).toBe(true);
