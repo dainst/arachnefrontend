38d3cef9bb7c598df23c85545615fc922136cc51
Richard Henck <richard.henck@dainst.de>
2018-01-04 16:52:23 +0100

    Sort places and add marker lines between them with arows indicating the chronological direction.
    Still work in progress, as it looks rather bad.

--- app/map/ar-entity-map.directive.js
+++ app/map/ar-entity-map.directive.js
@@ -147,7 +147,75 @@ angular.module('arachne.widgets.map')
 
                         map._onResize();
                     };
+
+                    var loadDirectionalArrows = function () {
+
+                        scope.places.sort(function(a, b) {
+                            var nameA = a.storageFromYear; // ignore upper and lowercase
+                            var nameB = b.storageToYear; // ignore upper and lowercase
+
+                            if (nameA == nameB) {
+                                return 0;
+                            } else {
+                                return nameA > nameB ? 1 : -1
+                            }
+                        });
+
+                        if (scope.places && scope.places.length > 1) {
+                            for (var i in scope.places) {
+                                var nextIndex = parseInt(i) + 1;
+
+                                // stop when last but one array element is reached
+                                if (nextIndex == scope.places.length) {
+                                    continue;
+                                }
+
+                                var place1 = scope.places[i];
+                                var place2 = scope.places[nextIndex];
+
+                                if (typeof place1.location == 'undefined' || typeof place2.location == 'undefined') {
+                                    // console.log('undefined location...skipping: ' + place1.name + ' <-> ' + place2.name);
+                                    continue;
+                                }
+
+                                // crashing when the locations are the same
+                                // -> JSON stringify to be able to compare the objects
+                                if (JSON.stringify(place1.location) == JSON.stringify(place2.location)) {
+                                    // console.log('same location...skipping: ' + place1.name + ' <-> ' + place2.name);
+                                    continue;
+                                }
+
+                                var latlngs2 = [
+                                    [place2.location.lat, place2.location.lon],
+                                    [place1.location.lat, place1.location.lon]
+                                ];
+
+                                var polyline = L.polyline(latlngs2, {color: 'red'}).addTo(map);
+
+                                // zoom the map to the polyline
+                                map.fitBounds(polyline.getBounds());
+
+                                var decorator = L.polylineDecorator(polyline, {
+                                    patterns: [{
+                                        offset: '100%',
+                                        // repeat: 10,
+                                        repeat: 0,
+                                        symbol: L.Symbol.arrowHead({
+                                            pixelSize: 30,
+                                            headAngle: 80,
+                                            pathOptions: {
+                                                color: 'red'
+                                            }
+                                        })
+                                    }]
+                                });
+                                decorator.addTo(map);
+                            }
+                        }
+                    }
+
                     loadMarkers();
+                    loadDirectionalArrows();
                 }
             };
         }]);
