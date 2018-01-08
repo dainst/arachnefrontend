b66a2aaee4b63b749134f801c1bd8083fccb88b6
Richard Henck <richard.henck@dainst.de>
2018-01-08 15:52:24 +0100

    Add colored lines between places nodes according to chronological order
    With this commit it works for single entities in the entity view. The necessary
    date data is needed in the database and elastioc search index respectively.
    
    The function filters out all the places which do not have a location information
     and also places with the same location twice right after another.
    
    The colored lines shown are a gradient moving from green at the finding spot to
    red at the last known location.

--- app/map/ar-entity-map.directive.js
+++ app/map/ar-entity-map.directive.js
@@ -148,74 +148,64 @@ angular.module('arachne.widgets.map')
                         map._onResize();
                     };
 
-                    var loadDirectionalArrows = function () {
+                    var loadDirectionalLines = function () {
+                        //TODO sorting even needed? if so, also use days and months for comparison
+                        // scope.places.sort(function(a, b) {
+                        //     var nameA = a.storageFromYear; // ignore upper and lowercase
+                        //     var nameB = b.storageToYear; // ignore upper and lowercase
+                        //
+                        //     if (nameA == nameB) {
+                        //         return 0;
+                        //     } else {
+                        //         return nameA > nameB ? 1 : -1
+                        //     }
+                        // });
 
-                        scope.places.sort(function(a, b) {
-                            var nameA = a.storageFromYear; // ignore upper and lowercase
-                            var nameB = b.storageToYear; // ignore upper and lowercase
-
-                            if (nameA == nameB) {
-                                return 0;
-                            } else {
-                                return nameA > nameB ? 1 : -1
+                        // Remove places without location value and places which have the same consecutive locations
+                        for (var i = 0; i < scope.places.length; i++) {
+                            if ((typeof scope.places[i].location == 'undefined') ||
+                                (i+1 < scope.places.length &&
+                                JSON.stringify(scope.places[i].location) == JSON.stringify(scope.places[i+1].location))) {
+                                scope.places.splice(i, 1);
+                                i--; // need to decrease the loop counter because the list just got smaller and
+                                     // the next object has the same index as this one
                             }
-                        });
+                        }
 
                         if (scope.places && scope.places.length > 1) {
-                            for (var i in scope.places) {
-                                var nextIndex = parseInt(i) + 1;
-
-                                // stop when last but one array element is reached
-                                if (nextIndex == scope.places.length) {
+                            for (var i = 0; i < scope.places.length; i++) {
+                                if (i + 1 == scope.places.length) { // stop when last but one array element is reached
                                     continue;
                                 }
 
                                 var place1 = scope.places[i];
-                                var place2 = scope.places[nextIndex];
+                                var place2 = scope.places[i+1];
 
-                                if (typeof place1.location == 'undefined' || typeof place2.location == 'undefined') {
-                                    // console.log('undefined location...skipping: ' + place1.name + ' <-> ' + place2.name);
-                                    continue;
-                                }
-
-                                // crashing when the locations are the same
-                                // -> JSON stringify to be able to compare the objects
-                                if (JSON.stringify(place1.location) == JSON.stringify(place2.location)) {
-                                    // console.log('same location...skipping: ' + place1.name + ' <-> ' + place2.name);
-                                    continue;
-                                }
-
-                                var latlngs2 = [
-                                    [place2.location.lat, place2.location.lon],
-                                    [place1.location.lat, place1.location.lon]
+                                var latlngs = [
+                                    [place2.location.lat, place2.location.lon, i+1],
+                                    [place1.location.lat, place1.location.lon, i]
                                 ];
 
-                                var polyline = L.polyline(latlngs2, {color: 'red'}).addTo(map);
+                                var hotlineOptions = {
+                                    min: 0,
+                                    max: scope.places.length - 1,
+                                    palette: {
+                                    0.0: '#008800',
+                                    0.5: '#ffff00',
+                                    1.0: '#ff0000'
+                                    },
+                                    weight: 2,
+                                    outlineWidth: 1
+                                }
 
-                                // zoom the map to the polyline
-                                map.fitBounds(polyline.getBounds());
-
-                                var decorator = L.polylineDecorator(polyline, {
-                                    patterns: [{
-                                        offset: '100%',
-                                        // repeat: 10,
-                                        repeat: 0,
-                                        symbol: L.Symbol.arrowHead({
-                                            pixelSize: 30,
-                                            headAngle: 80,
-                                            pathOptions: {
-                                                color: 'red'
-                                            }
-                                        })
-                                    }]
-                                });
-                                decorator.addTo(map);
+                                var hotlineLayer = L.hotline(latlngs, hotlineOptions).addTo(map);
+                                map.fitBounds(hotlineLayer.getBounds());
                             }
                         }
                     }
 
                     loadMarkers();
-                    loadDirectionalArrows();
+                    loadDirectionalLines();
                 }
             };
         }]);
