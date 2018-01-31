ad73b2f6a955dc48d5f86a0c7cbcb8b4a906d40e
Paflov <philipp.franck@dainst.de>
2018-01-31 15:38:01 +0100

    e2e: removes fits

--- test/e2e/map.spec.js
+++ test/e2e/map.spec.js
@@ -2,14 +2,14 @@ var EC = protractor.ExpectedConditions;
 var map = require('./map/map.page');
 
 describe('map', function() {
-    fit('should include a heatmap on the grako_map page', function() {
+    it('should include a heatmap on the grako_map page', function() {
         browser.get('/project/grako_map?lang=de').then(function () {
             var heatmap = map.getHeatmap();
             expect(heatmap.isPresent()).toBe(true);
         });
     });
 
-    fit('it should show some markers for small result size', function () {
+    it('it should show some markers for small result size', function () {
         browser.get('/map?zoom=12&lat=50.42116487566384&lng=4.902398681640625').then(function () {
             var marker = map.getMarkers();
        	    expect(marker.count()).toBeGreaterThanOrEqual(1);
