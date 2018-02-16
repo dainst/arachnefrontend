24ba72870306dab9fe3bbd9f792353d613caf447
Paflov <philipp.franck@dainst.de>
2018-02-16 18:16:04 +0100

    repair broken test

--- test/e2e/scope-search.spec.js
+++ test/e2e/scope-search.spec.js
@@ -56,7 +56,7 @@ describe('scoped search result page', function() {
 
 	it('shall stay in scope when switched to normal search view in scoped search', function() {
 		searchPage.loadScoped('grako', 'map', {q:"Florenz"})
-			.then(mapPage.switchToSearchView())
+			.then(common.switchView('tiles'))
             .then(function(newUrl) {
                 expect(newUrl).toContain("project/grako/search?q=Florenz");
             });
