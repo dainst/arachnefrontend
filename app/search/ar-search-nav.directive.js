bb66222dc9c83b2b8da6a355108e4c8d5314a9b0
Paflov <philipp.franck@dainst.de>
2018-02-16 14:40:58 +0100

    removes fl parameter from links in view switcher
    because otherwise there is a problem when you switch from map back to tiles-view

--- app/search/ar-search-nav.directive.js
+++ app/search/ar-search-nav.directive.js
@@ -4,11 +4,6 @@ angular.module('arachne.directives')
 
     .directive('arSearchNav', function () {
         return {
-            templateUrl: 'app/search/ar-search-nav.html',
-            scope: true,
-            link: function(scope) {
-                scope.hidePagination = scope.hidePagination || false;
-                scope.hideSortOptions = scope.hideSortOptions || false;
-            }
+            templateUrl: 'app/search/ar-search-nav.html'
         }
     });
