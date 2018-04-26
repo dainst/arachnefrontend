234850ea05c6525ee61efe241910e16c5cad22a6
Sebastian Cuy <sebastian.cuy@dainst.de>
2018-04-26 17:38:02 +0200

    include cookie notice
    closes #8345

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,8 @@ var jsDeps = [
     'node_modules/angular-ui-swiper/dist/angular-ui-swiper.min.js',
     'node_modules/angular-md5/angular-md5.js',
     'node_modules/svg-pan-zoom/dist/svg-pan-zoom.js',
-    'node_modules/leaflet-fullscreen/dist/Leaflet.fullscreen.min.js'
+    'node_modules/leaflet-fullscreen/dist/Leaflet.fullscreen.min.js',
+    'node_modules/idai-cookie-notice/idai-cookie-notice.js'
 ];
 
 // compile sass and concatenate to single css file in build dir
