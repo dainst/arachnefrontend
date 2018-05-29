e3344c94008a7b8f88e78a10bfd9d59ed631a48a
Simon Hohl <simon.hohl@dainst.org>
2018-05-29 16:39:58 +0200

    Stop AngularJS from redirecting from CSV files to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -284,7 +284,7 @@ gulp.task('server', function () {
             middleware: [
                 proxy(proxyOptions),
                 // rewrite for AngularJS HTML5 mode, redirect all non-file urls to index.html
-                modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.gif|\\.json|\\.woff2|\\.woff|\\.ttf$ /index.html [L]'])
+                modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.gif|\\.json|\\.csv|\\.woff2|\\.woff|\\.ttf$ /index.html [L]'])
             ]
         },
         port: 8082,
