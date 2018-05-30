7e1a1a20e42fd99ebf2304be6adbd0d01785b4ab
Simon Hohl <simon.hohl@dainst.org>
2018-05-30 10:03:01 +0200

    Add notes for debugging Vega

--- app/visualizations/vega.directive.js
+++ app/visualizations/vega.directive.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// In order to debug your Vega spec, run the following command in your browser's console:
+// view = angular.element(document.getElementsByName('<name attribute>')).scope().$$childHead.vegaView
+// You can then use the variable view as described in https://vega.github.io/vega/docs/api/debugging/
+
 angular.module('arachne.visualizations.directives')
     .directive('con10tVega', function () { // con10t-vega
         return {
@@ -14,6 +18,7 @@ angular.module('arachne.visualizations.directives')
                     scope.vegaView = new vega.View(vega.parse(spec))
                         .renderer('canvas')
                         .initialize('[name="' + scope.name + '"]')
+                        //.logLevel(vega.Debug)
                         .hover()
                         .run();
                 };
