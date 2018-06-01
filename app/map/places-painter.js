faf8bac957d57f88a7e1c27d724921504f2fc0e4
paflov <philipp.franck@dainst.de>
2018-06-01 15:41:20 +0200

    Made hidden places avaibale again. There where hidden when twi places have the same coordinates. Alos refactores the places-painter a lot.

--- app/map/places-painter.js
+++ app/map/places-painter.js
@@ -6,7 +6,7 @@ angular.module('arachne.widgets.map')
  * @author: David Neugebauer
  * @author: Daniel de Oliveira
  */
-.factory('placesPainter', ['$compile', 'Place', function ($compile, Place) {
+.factory('placesPainter', ['$compile', 'Place', '$rootScope', function ($compile, Place, $rootScope) {
 
     var markers = null; // used to keep track of markers for deleting them later
     var translocationLayerGroups = [];
@@ -19,6 +19,71 @@ angular.module('arachne.widgets.map')
         lonmin: 180,
         lonmax: 0
 	};
+
+    var idaiCircleMarker = L.CircleMarker.extend({
+        place: {},
+        setPlace: function(place) {
+            this.place = place;
+            return this;
+        }
+    });
+
+    // for clusters svg markers are not provided unfortunately
+    var idaiClusterMarker = L.DivIcon.extend({
+        options: {
+            html: '',
+            value: undefined,
+            className: "cluster-marker",
+            iconSize: [16, 16]
+        },
+        render: function(stats) {
+            var color = angular.isUndefined(stats) ? 'rgb(0,255,0)' : calculateMarkerColor(this.options.value, stats.min, stats.max, stats.mean);
+            this.options.html = '<div class="cluster-marker-inner" style="background-color:' + color + '">&nbsp;</div>';
+            return this;
+        }
+    });
+
+    function createPlaceMarker(place) {
+        if (!place.hasCoordinates()) {
+            console.warn("place not displayed due to missing coordinates:", place);
+            return;
+        }
+
+        return new idaiCircleMarker(
+            new L.LatLng(place.location.lat, place.location.lon),
+            {
+                radius: 6,
+                fillOpacity: 1,
+                opacity: 1,
+                weight: 1,
+                color: '#000000',
+                className: 'circleMarker',
+                fillColor: '#000000',
+                riseOnHover: true
+            }
+        )
+            .setPlace(place)
+            .bindPopup('dummy'); //otherwise spiderfied markers have no popup first time they are clicked
+    }
+
+    function createMarkerPopup(place) {
+        if (place.isFixed === true) {
+            var title = (place.gazetteerId) ?
+                '<strong><a href="https://gazetteer.dainst.org/app/#!/show/' + place.gazetteerId + '" target="_blank">' + place.name + '</a></strong>' :
+                '<strong>' + place.name + '</strong>';
+            var body = (place.text) ?
+                '<p>' + place.text + '</p>' :
+                '';
+            return title + body;
+        }
+
+        var newScope = $rootScope.$new(true);
+        newScope.place = place;
+        newScope.entityCallback = entityCallback;
+        var html = '<div con10t-map-popup place="place" entity-callback="entityCallback"></div>';
+        return $compile(angular.element(html))(newScope)[0];
+    }
+
 
     function getPlacesStats(places) {
         var stats = places.reduce(function(acc, cur) {
@@ -34,6 +99,7 @@ angular.module('arachne.widgets.map')
         });
         stats.count = places.length;
         stats.mean = stats.sum / stats.count;
+
         return stats;
     }
 
@@ -68,6 +134,84 @@ angular.module('arachne.widgets.map')
             .map(calcValue)
             .map(toHex)
             .join("");
+    }
+
+    function createMarkerClusterGroup() {
+        var options = {
+            spiderfyDistanceMultiplier: 1.1,
+            showCoverageOnHover: false,
+            zoomToBoundsOnClick: false,
+            spiderfyOnMaxZoom: true,
+            cluserPane: "shadow-pane",
+            spiderLegPolylineOptions: {
+                weight: 2,
+                color: '#000',
+                opacity: 0.5
+            },
+            maxClusterRadius: 15,
+            iconCreateFunction: function createClusterMarker(cluster) {
+                return new idaiClusterMarker().render();
+            }
+        };
+
+        return new L.MarkerClusterGroup(options);
+    }
+
+    function colorize(clusterLayer) {
+
+        var topCluster = clusterLayer._topClusterLevel;
+        var visibleClusters = [];
+        var allClusters = [];
+
+        function sum(a, b) {
+            return a + b;
+        }
+
+        function countAllClustersEntitiesRecursive(cluster) {
+            // don't get fooled by cluster._childClusters.length
+            var childCount = cluster._childClusters
+                    .map(countAllClustersEntitiesRecursive)
+                    .reduce(sum, 0)
+                + cluster._markers
+                    .map(function(marker) {
+                        return marker.place.entityCount
+                    })
+                    .reduce(sum, 0);
+
+
+            var clusterAndCount = {
+                cluster: cluster,
+                entityCount: childCount
+            };
+            allClusters.push(clusterAndCount);
+
+            if (map.getZoom() === cluster._zoom) {
+                visibleClusters.push(clusterAndCount);
+            }
+
+
+            return childCount;
+        }
+
+        countAllClustersEntitiesRecursive(topCluster);
+
+        var placesStats = getPlacesStats(visibleClusters);
+
+        // we do not only paint visible clusters but all, because
+        // zooming looks very bad otherwise, since the clusters seems to become green until the new data arrives.
+        // this could be avoided if markers would get removed right after zoomend and not stay until new data
+        // arrived. or a way faster backend.
+        visibleClusters.forEach(function(entry) {
+            entry.cluster.setIcon(new idaiClusterMarker({value: entry.entityCount}).render(placesStats));
+        });
+
+        topCluster.getAllChildMarkers().forEach(function(marker) {
+            var color = calculateMarkerColor(marker.place.entityCount, placesStats.min, placesStats.max, placesStats.mean);
+
+            marker.setStyle({
+                fillColor: color
+            })
+        });
     }
 
 
@@ -117,72 +261,33 @@ angular.module('arachne.widgets.map')
             return (fixedPlaces.length) ? boundingBox : false;
         },
 
-
-
-        // create the actual places' markers
         drawPlaces: function(places, scope) {
 
             if (!places) return;
 
-            markers = L.featureGroup().addTo(map);
-
             var mergedPlaces = fixedPlaces.concat(places);
-            var stats = getPlacesStats(mergedPlaces);
+
+            markers = createMarkerClusterGroup().addTo(map);
 
             for (var i = 0; i < mergedPlaces.length; i++) {
-                var place = mergedPlaces[i];
-
-                if (!place.hasCoordinates()) {
-                    continue;
-                }
-
-                // Dom-Element für Popup bauen und in Link-Funktion kompilieren
-                var newScope = scope.$new(true);
-
-                newScope.place = place;
-                if (place.isFixed === true) {
-                    var linkFunction = function(scope) {
-                        var title = (scope.place.gazetteerId) ?
-                            '<strong><a href="https://gazetteer.dainst.org/app/#!/show/' + scope.place.gazetteerId + '" target="_blank">' + scope.place.name + '</a></strong>' :
-                            '<strong>' + scope.place.name + '</strong>';
-                        var body = (scope.place.text) ?
-                            '<p>' + scope.place.text + '</p>' :
-                            '';
-                    return [title + body];
-                    }
-                } else {
-                    var html = '<div con10t-map-popup place="place" entity-callback="entityCallback"></div>';
-                    var linkFunction = $compile(angular.element(html));
-                    newScope.entityCallback = entityCallback;
-                }
-
-                var markerType = 'default';
-
-                var marker = new L.CircleMarker(
-                    new L.LatLng(place.location.lat, place.location.lon),
-                    {
-                        radius:			6,
-                        fillOpacity:	0.7,
-                        opacity:		1,
-                        weight:         1,
-                        color:          '#000000',
-                        className:      'circleMarker',
-                        fillColor:      calculateMarkerColor(place.entityCount, stats.min, stats.max, stats.mean)
-                    }
-                ).addTo(markers);
-
-                marker.on('click', function(newScope,linkFunction) {
-                    var popup;
-                    return function(e) {
-                        if (!popup) popup = linkFunction(newScope)[0];
-                        if (e.target.getPopup()) e.target.unbindPopup();
-                        e.target.bindPopup(popup,{ minWidth: 150, autoPan: false });
-                        e.target.openPopup();
-                    };
-                }(newScope,linkFunction));
-
+                createPlaceMarker(mergedPlaces[i]).addTo(markers);
             }
-            markers.bringToFront();
+
+            markers
+                .on('click', function(clickEvent) {
+                    var marker = clickEvent.layer;
+                    var popup = createMarkerPopup(marker.place);
+                    marker.bindPopup(popup, {
+                        minWidth: 150
+                    });
+                    marker.openPopup();
+                })
+                .on('clusterclick', function(clickEvent) {
+                    clickEvent.layer.spiderfy();
+                })
+                .bringToFront();
+
+            colorize(markers);
         },
 
         clearTranslocationLines: function() {
