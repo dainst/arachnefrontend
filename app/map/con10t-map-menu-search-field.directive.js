20e9431a6cbaca5551378a33785e8ee0e57a3774
paflov <philipp.franck@dainst.de>
2018-06-04 18:09:54 +0200

    project map page: load results when pressed enter in internal search box

--- app/map/con10t-map-menu-search-field.directive.js
+++ app/map/con10t-map-menu-search-field.directive.js
@@ -5,8 +5,8 @@ angular.module('arachne.widgets.map')
 /**
  * @author: David Neugebauer
  */
-.directive('con10tMapMenuSearchField', ['$location', 'searchService', 'mapService',
-function($location, searchService, mapService) {
+.directive('con10tMapMenuSearchField', ['$location', 'searchService', 'mapService', '$window',
+function($location, searchService, mapService, $window) {
 return {
     restrict: 'A',
     scope: {
@@ -19,19 +19,20 @@ return {
         var currentQuery = searchService.currentQuery();
 
         searchService.getCurrentPage().then(function() {
-            scope.q = currentQuery.q
-            if (scope.q == '*') scope.q = '';
+            scope.q = currentQuery.q;
+            if (scope.q === '*') scope.q = '';
         });
 
         scope.search = function(q) {
-            if (q == '') q = '*';
+            if (q === '') q = '*';
 
             // remove coordinate and zoom params on new search to indicate that the map
             // should choose its default action when rendering the new objects' places
             var query = mapService.getMapQuery(searchService.currentQuery(),true).setParam('q',q);
 
             var path = '/' + route + query.toString();
-            $location.url(path);
+            $window.location.href = path;
+
         };
     }
 }
