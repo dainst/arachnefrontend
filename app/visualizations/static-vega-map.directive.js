d8ba2e39f1367b369a7b84d08261803c7926cb7b
Simon Hohl <simon.hohl@dainst.org>
2018-06-27 13:58:12 +0200

    Make latitude, longitude and zoom-level directive attributes

--- app/visualizations/static-vega-map.directive.js
+++ app/visualizations/static-vega-map.directive.js
@@ -10,25 +10,28 @@ angular.module('arachne.visualizations.directives')
             restrict: 'E',
             scope: {
                 spec: '@',
+                lat: '@',
+                lng: '@',
+                zoom: '@',
                 name: '@'
             },
             link: function (scope, element, attrs) {
 
                 scope.renderVega = function (spec) {
-                    scope.map = L.map('map').setView([40.7128, -74.0059], 4);
+                    scope.map = L.map('map').setView([scope.lat, scope.lng], scope.zoom);
 
                     L.tileLayer(
                         'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=b47a3cf895b94aedad41e5cfb5222b87', { })
                         .addTo(scope.map);
 
-                    var icon = 'user';
-                    var awesomeMarker = L.AwesomeMarkers.icon({
-                        icon: icon,
-                        markerColor: 'cadetblue'
-                    });
-
-                    L.marker(new L.LatLng(40.7128, -74.0059), {title: 'Example Marker', icon: awesomeMarker}).addTo(scope.map)
-                        .bindPopup('A Leaflet marker popup<br><b>(on top of Vega)</b>');
+                    // var icon = 'user';
+                    // var awesomeMarker = L.AwesomeMarkers.icon({
+                    //     icon: icon,
+                    //     markerColor: 'cadetblue'
+                    // });
+                    //
+                    // L.marker(new L.LatLng(40.7128, -74.0059), {title: 'Example Marker', icon: awesomeMarker}).addTo(scope.map)
+                    //     .bindPopup('A Leaflet marker popup<br><b>(on top of Vega)</b>');
 
                     scope.layer = L.vega(spec, {
                        // Make sure the legend stays in place when moving (slower)
