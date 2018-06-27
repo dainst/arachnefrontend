cebf0f7352f9f832893b39fe8b247b49feac9182
Simon Hohl <simon.hohl@dainst.org>
2018-06-27 13:43:38 +0200

    Add vega-as-leaflet-layer demo code as an AngularJS directive
    In contrast to the official demo, the Vega specification is not
    specified inline. Instead, the path to a spec file has to be provided,
    similar to the default static-vega directive.
    
    Original demo code: https://github.com/nyurik/leaflet-vega/blob/34b6433e302cc8caa89fd332ae235c31adb87303/demo/demo.html

---
+++ app/visualizations/static-vega-map.directive.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// In order to debug your Vega spec, run the following command in your browser's console:
+// view = angular.element(document.getElementsByName('<name attribute>')).scope().$$childHead.vegaView
+// You can then use the variable view as described in https://vega.github.io/vega/docs/api/debugging/
+
+angular.module('arachne.visualizations.directives')
+    .directive('con10tStaticVegaMap', function(){
+        return {
+            restrict: 'E',
+            scope: {
+                spec: '@',
+                name: '@'
+            },
+            link: function (scope, element, attrs) {
+
+                scope.renderVega = function (spec) {
+                    scope.map = L.map('map').setView([40.7128, -74.0059], 4);
+
+                    L.tileLayer(
+                        'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=b47a3cf895b94aedad41e5cfb5222b87', { })
+                        .addTo(scope.map);
+
+                    var icon = 'user';
+                    var awesomeMarker = L.AwesomeMarkers.icon({
+                        icon: icon,
+                        markerColor: 'cadetblue'
+                    });
+
+                    L.marker(new L.LatLng(40.7128, -74.0059), {title: 'Example Marker', icon: awesomeMarker}).addTo(scope.map)
+                        .bindPopup('A Leaflet marker popup<br><b>(on top of Vega)</b>');
+
+                    scope.layer = L.vega(spec, {
+                       // Make sure the legend stays in place when moving (slower)
+                       delayRepaint: false
+                    });
+
+                    scope.layer.addTo(scope.map);
+                };
+
+                vega.loader()
+                    .load(scope.spec)
+                    .then(function(data){
+                        scope.renderVega(JSON.parse(data));
+                    });
+            }
+        }
+});
