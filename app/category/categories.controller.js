6bf877c8519c5c51d459f8c9b27c9230b44e29cd
Sven Wolter <sven.wolter@dainst.de>
2018-08-29 17:56:20 +0200

    #9312 - ugrade d3 package to v5.7.0

--- app/category/categories.controller.js
+++ app/category/categories.controller.js
@@ -8,30 +8,30 @@ angular.module('arachne.controllers')
 
             categoryService.getCategoriesAsync().then(function (categories) {
                 $scope.categories = [];
-                for (var key in categories) {
-                    if (categories[key].status != 'none') {
+                angular.forEach(Object.keys(categories), function(key) {
+                    console.log(categories[key]);
+                    if (categories[key].status !== 'none') {
                         $scope.categories.push(categories[key]);
                     }
-                }
+                });
                 drawGraph();
             });
 
             function drawGraph () {
-                d3.json("config/category_relations.json", function(classes) {
-
+                d3.json("config/category_relations.json").then(function(classes) {
                     var category_graph = document.getElementById('category_graph');
                     category_graph.setAttribute('width', (window.innerWidth/2));
                     category_graph.setAttribute('height', (window.innerWidth/2) - 50);
                     
                     // move images to lower part of the graph
                     for (var i = 0, length = classes.length; i < length; i++) {
-                        if (classes[i].name == "marbilder") {
+                        if (classes[i].name === "marbilder") {
                             classes.splice(4, 0, classes.splice(i, 1)[0]);
                             break;
                         }
                     }
 
-                    $scope.graphClasses = classes
+                    $scope.graphClasses = classes;
                     var matrix = generateMatrix(classes);
 
                     var svg = d3.select("svg"),
@@ -66,15 +66,16 @@ angular.module('arachne.controllers')
                       .enter().append("g");
 
                     group.append("path")
-                        .style("fill", function(d) { return classes[d.index].name == "marbilder" ? "rgb(230, 230, 230)" : d3.rgb(color(0)); })
-                        .style("stroke", function(d) { return d3.rgb(color(0)).darker(30); })
+                        .style("fill", function(d) {
+                            return classes[d.index].name === "marbilder" ? "rgb(230, 230, 230)" : d3.rgb(color(0)); })
+                        .style("stroke", function() { return d3.rgb(color(0)).darker(30); })
                         .attr("class", function(d) { return "bar bar-" + classes[d.index].name; })
                         .style("cursor", "pointer")
                         .attr("d", arc)
                         .on("mouseover", graph_mouseover)
                         .on("mousedown", graph_mousedown);
 
-                    var groupText = group.append("text")
+                    group.append("text")
                         .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
                         .attr("dy", ".35em")
                         .attr("transform", function(d) {
@@ -93,33 +94,38 @@ angular.module('arachne.controllers')
                         .data(function(chords) { return chords; })
                       .enter().append("path")
                         .attr("d", ribbon)
-                        .attr("id", function(d) { return "link-" + classes[d.source.index].name + classes[d.target.index].name })
-                        .attr("class", function(d) { return "link source-" + classes[d.source.index].name + " target-" + classes[d.target.index].name; })
-                        .style("fill", function(d) { return d3.rgb(color(0)).brighter(); })
+                        .attr("id", function(d) {
+                            return "link-" + classes[d.source.index].name + classes[d.target.index].name })
+                        .attr("class", function(d) {
+                            return "link source-" +
+                                classes[d.source.index].name +
+                                " target-" + classes[d.target.index].name; })
+                        .style("fill", function() { return d3.rgb(color(0)).brighter(); })
                         .attr("opacity", 0.8)
-                        .style("stroke", function(d) { return d3.rgb(color(0)); });
+                        .style("stroke", function() { return d3.rgb(color(0)); });
 
-                    $scope.svg = svg
+                    $scope.svg = svg;
 
                     
-                    for (var categoryBoxes = document.getElementsByClassName('ar-imagegrid-cell-link'), i = 0, leength = categoryBoxes.length; i < leength; i++) {
-                        categoryBoxes[i].addEventListener("mouseover", function () {
+                    for (var categoryBoxes = document.getElementsByClassName('ar-imagegrid-cell-link'),
+                             j = 0, leength = categoryBoxes.length; j < leength; j++) {
+                        categoryBoxes[j].addEventListener("mouseover", function () {
                             svg.selectAll("path.link").attr("opacity", 0);
                             var classname = this.attributes['href'].value.split('c=')[1];
                             this.setAttribute('id',classname + "_box");
-                            svg.selectAll("path.link.source-" + classname).attr("opacity", 0.8)
+                            svg.selectAll("path.link.source-" + classname).attr("opacity", 0.8);
                             svg.selectAll("path.link.target-" + classname).attr("opacity", 0.8)
                         });
 
                     }
                     document.getElementById('categories_container').addEventListener("mouseout", function () {
                          svg.selectAll("path.link").attr("opacity", 0.8);
-                    })
+                    });
                     document.getElementById('categories_graph_container').addEventListener("mouseout", function () {
                         svg.selectAll("path.link").attr("opacity", 0.8);
                         for (var catBoxes = document.getElementsByClassName('ar-imagegrid-cell-link'),
                                 leength = catBoxes.length, i = 0; i < leength; i++) {
-                            catBoxes[i].style.opacity = 1.0
+                            catBoxes[i].style.opacity = 1.0;
                             catBoxes[i].classList.remove('hover')
                         }
                         svg.selectAll("path.bar").attr("opacity", 1.0);
@@ -143,11 +149,11 @@ angular.module('arachne.controllers')
                 
                 for (var catBoxes = document.getElementsByClassName('ar-imagegrid-cell-link'),
                         i = 0, leength = catBoxes.length; i < leength; i++) {
-                    if( catBoxes[i].attributes['href'].value == "category/?c=" + classname) {
-                        catBoxes[i].style.opacity = 1.0
+                    if( catBoxes[i].attributes['href'].value === "category/?c=" + classname) {
+                        catBoxes[i].style.opacity = 1.0;
                         catBoxes[i].classList.add('hover')
                     } else {
-                        catBoxes[i].style.opacity = 0.3
+                        catBoxes[i].style.opacity = 0.3;
                         catBoxes[i].classList.remove('hover')
                     }
                 }
@@ -159,25 +165,25 @@ angular.module('arachne.controllers')
             }
 
             function generateMatrix(classes) {
-                var nameToIndex = {}
-                var matrix = []
+                var nameToIndex = {};
+                var matrix = [];
                 for (var i = 0, leength = classes.length; i < leength; i++) {
-                    var klass = classes[i]
+                    var klass = classes[i];
 
-                    nameToIndex[klass.name] = i
-                    matrix[i] = []
+                    nameToIndex[klass.name] = i;
+                    matrix[i] = [];
 
                     for (var u = 0; u < classes.length; u++) {
                         matrix[i][u] = 0
                     }
 
                     for (var z = 0; z < classes.length; z++) {
-                        var klassname = classes[z].name
-                        if (klass.connections[klassname] != undefined) {
+                        var klassname = classes[z].name;
+                        if (klass.connections[klassname] !== undefined) {
                             matrix[i][z] = klass.connections[klassname]
                         }
                     }   
-                };
+                }
                 return matrix
             }
         }        
