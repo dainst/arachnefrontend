ac06010c51f46267f60217ff2f4a213c75fe6a82
paflov <philipp.franck@dainst.de>
2018-11-10 00:37:30 +0100

    adds the Download-Link and some other improvements to the Export-View

--- app/pages/dataexport.controller.js
+++ app/pages/dataexport.controller.js
@@ -3,24 +3,37 @@
 angular.module('arachne.controllers')
 
 
-.controller('DataexportController', ['$scope', '$http', '$location', '$interval', 'arachneSettings',
-    function ($scope, $http, $location, $interval, arachneSettings) {
+.controller('DataexportController', ['$scope', '$http', '$timeout', 'arachneSettings', 'authService',
+    function ($scope, $http, $timeout, arachneSettings, authService) {
 
-        $scope.status = [];
+        $scope.status = {
+            tasks: {}
+        };
+
+        $scope.dataserviceUri = arachneSettings.dataserviceUri;
+
+        $scope.user = authService.getUser();
+
+        $scope.hasTasks = function() {
+             return (Object.keys($scope.status.tasks).length > 0);
+        };
 
         function fetchStatus() {
             $http.get(arachneSettings.dataserviceUri + '/export/status').then(
                 function(response) {
                     console.log('res', response);
                     $scope.status = response.data;
+                    $timeout.cancel(timeout);
+                    timeout = $timeout(fetchStatus, 10000);
                 },
                 function(response) {
-                    console.log('ERRRres', response);
+                    console.warn('Error:', response);
                 }
             );
         }
 
-        fetchStatus();
-
-
+        var timeout = $timeout(fetchStatus, 1);
+        $scope.$on('$destroy', function(){
+            $timeout.cancel(timeout);
+        });
     }]);
