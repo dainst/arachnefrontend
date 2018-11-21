3f39a135865945baf25285aa4a267180d10cb7b1
Richard Henck <richard.henck@dainst.de>
2018-11-21 10:46:26 +0100

    Revert "Try alternative browser test settings to fix E2E tests"
    This reverts commit 607ab1dd4c27fed52f963948507800a33980b071.

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -13,11 +13,7 @@ exports.config = {
     exclude: [],
     chromeOnly: true,
     multiCapabilities: [{
-        browserName: 'chrome',
-        chromeOptions: {
-            binary: '/usr/bin/chromium-browser',
-            args: ["--headless", "--disable-gpu", "--window-size=1440x900", "--no-sandbox"]
-        }
+        browserName: 'chrome'
     }],
     allScriptsTimeout: 110000,
     getPageTimeout: 100000,
