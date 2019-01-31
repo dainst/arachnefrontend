724a5765c0fa62907af3e645c5df54fea414b552
Simon Hohl <simon.hohl@dainst.org>
2019-01-31 13:00:41 +0100

    Create first version of paginated item list
    See Redmine ticket #10458.

---
+++ app/visualizations/con10t-paginated-item-list.directive.js
@@ -0,0 +1,56 @@
+'use strict';
+
+angular.module('arachne.visualizations.directives')
+    .directive('con10tPaginatedItemList', ['$filter', function ($filter) {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/visualizations/con10t-paginated-item-list.html',
+            scope: {
+                heading: '@',
+                itemsPerPage: '@',
+                itemList: '=',
+                selected: '='
+            },
+            link: function (scope, element, attrs) {
+
+                scope.offset = 0;
+                scope.ipp = parseInt(scope.itemsPerPage);
+
+                scope.increaseOffset = function(){
+                    if(scope.offset + scope.ipp < scope.itemList.length - scope.ipp){
+                        scope.offset += scope.ipp;
+                    } else {
+                        scope.offset = scope.itemList.length - scope.ipp;
+                    }
+                };
+
+                scope.decreaseOffset = function(){
+                    if(scope.offset - scope.ipp >= 0){
+                        scope.offset -= scope.ipp;
+                    } else {
+                        scope.offset = 0
+                    }
+                };
+
+                scope.toggleItem = function(id){
+                    var index = scope.selected.indexOf(id);
+                    if(index >= 0){
+                        scope.selected.splice(index, 1);
+                    } else {
+                        scope.selected.push(id)
+                    }
+                };
+
+                scope.deselectAll = function(){
+                    scope.selected = [];
+                };
+
+                scope.selectAll = function(){
+                    scope.selected = [];
+                    for(var key in scope.itemList){
+                        scope.selected.push(scope.itemList[key][0]);
+                    }
+                }
+            }
+        }
+    }]);
